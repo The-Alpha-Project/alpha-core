3414a54f0ae49bd02f39db0606ae460e0f5fe0aa
GrenderG <grenderg@gmail.com>
2020-02-01 04:23:29 +0100

    Reset update packet stuff on every packet building.

--- network/packet/UpdatePacketFactory.py
+++ network/packet/UpdatePacketFactory.py
@@ -9,6 +9,21 @@ class UpdatePacketFactory(object):
     def __init__(self, types_list=()):
         self.types_list = types_list
 
+        self.object_values = []
+        self.item_values = []
+        self.container_values = []
+        self.unit_values = []
+        self.player_values = []
+        self.gameobject_values = []
+        self.dynamic_object_values = []
+        self.init_lists()
+
+        self.update_packet = b''
+
+    def update(self, values_list, pos, value, value_type):
+        values_list.insert(pos, pack('!%s' % value_type, value))
+
+    def init_lists(self):
         self.object_values = [pack('!I', 0)] * ObjectFields.OBJECT_END.value
         self.item_values = [pack('!I', 0)] * ItemFields.ITEM_END.value
         self.container_values = [pack('!I', 0)] * ContainerFields.CONTAINER_END.value
@@ -17,12 +32,9 @@ class UpdatePacketFactory(object):
         self.gameobject_values = [pack('!I', 0)] * GameObjectFields.GAMEOBJECT_END.value
         self.dynamic_object_values = [pack('!I', 0)] * DynamicObjectFields.DYNAMICOBJECT_END.value
 
+    def build_packet(self):
         self.update_packet = b''
 
-    def update(self, values_list, pos, value, value_type):
-        values_list.insert(pos, pack('!%s' % value_type, value))
-
-    def build_packet(self):
         if ObjectTypes.TYPE_OBJECT.value in self.types_list:
             self.update_packet += b''.join(self.object_values)
         if ObjectTypes.TYPE_UNIT.value in self.types_list:
@@ -38,4 +50,5 @@ class UpdatePacketFactory(object):
         if ObjectTypes.TYPE_DYNAMICOBJECT.value in self.types_list:
             self.update_packet += b''.join(self.dynamic_object_values)
 
+        self.init_lists()
         return self.update_packet
