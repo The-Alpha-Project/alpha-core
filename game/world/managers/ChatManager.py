faef0d3b011dde5717f1a936458e86d5f1390a6c
GrenderG <grenderg@gmail.com>
2020-02-08 23:54:11 +0100

    Refactor ChatManager.py.

--- game/world/managers/ChatManager.py
+++ game/world/managers/ChatManager.py
@@ -9,27 +9,26 @@ class ChatManager(object):
 
     @staticmethod
     def send_system_message(world_session, message):
-        message_bytes = PacketWriter.string_to_bytes(message)
-        data = pack(
-            '<BIQ%usB' % len(message_bytes),
-            ChatMsgs.CHAT_MSG_SYSTEM.value,
-            0,
-            world_session.player_mgr.guid,
-            message_bytes,
-            ChatFlags.CHAT_TAG_NONE.value
-        )
-        world_session.request.sendall(PacketWriter.get_packet(OpCode.SMSG_MESSAGECHAT, data))
+        world_session.request.sendall(ChatManager._get_message_packet(world_session.player_mgr.guid,
+                                                                      ChatFlags.CHAT_TAG_NONE.value,
+                                                                      message, ChatMsgs.CHAT_MSG_SYSTEM, 0))
 
     @staticmethod
     def send_chat_message(world_session, message, chat_type, lang, range_):
+        GridManager.send_surrounding_in_range(ChatManager._get_message_packet(world_session.player_mgr.guid,
+                                                                              world_session.player_mgr.chat_flags,
+                                                                              message, chat_type, lang),
+                                              world_session.player_mgr, range_)
+
+    @staticmethod
+    def _get_message_packet(guid, chat_flags, message, chat_type, lang):
         message_bytes = PacketWriter.string_to_bytes(message)
         data = pack(
             '<BIQ%usB' % len(message_bytes),
             chat_type.value,
-            0,  # lang, disregard for now––not implemented
-            world_session.player_mgr.guid,
+            lang,
+            guid,
             message_bytes,
-            world_session.player_mgr.chat_flags
+            chat_flags
         )
-        GridManager.send_surrounding_in_range(PacketWriter.get_packet(OpCode.SMSG_MESSAGECHAT, data),
-                                              world_session.player_mgr, range_)
+        return PacketWriter.get_packet(OpCode.SMSG_MESSAGECHAT, data)
