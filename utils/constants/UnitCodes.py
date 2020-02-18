96921af4714e90779037f7c3ec53d6bb219dfcd5
GrenderG <grenderg@gmail.com>
2020-02-18 17:32:32 +0100

    Added some flags from 1.12.

--- utils/constants/UnitCodes.py
+++ utils/constants/UnitCodes.py
@@ -102,6 +102,7 @@ class SheathState(Enum):
     SHEATH_STATE_RANGED = 2
 
 
+# This is from 1.12, might be wrong
 class ObjectSpawnFlags(Enum):
     SPAWN_FLAG_ACTIVE = 0x01
     SPAWN_FLAG_DISABLED = 0x02
@@ -112,48 +113,8 @@ class ObjectSpawnFlags(Enum):
     SPAWN_FLAG_NOT_VISIBLE = 0x40  # creature only
 
 
-# TODO NOTE: BELOW VALUES ARE TAKEN FROM 1.12 AND THEY SEEM TO BE WRONG
-# used in most movement packets (send and received)
-class MovementFlags(IntFlag):
-    MOVEFLAG_NONE = 0x00000000
-    MOVEFLAG_FORWARD = 0x00000001
-    MOVEFLAG_BACKWARD = 0x00000002
-    MOVEFLAG_STRAFE_LEFT = 0x00000004
-    MOVEFLAG_STRAFE_RIGHT = 0x00000008
-    MOVEFLAG_TURN_LEFT = 0x00000010
-    MOVEFLAG_TURN_RIGHT = 0x00000020
-    MOVEFLAG_PITCH_UP = 0x00000040
-    MOVEFLAG_PITCH_DOWN = 0x00000080
-    MOVEFLAG_WALK_MODE = 0x00000100  # Walking
-    # MOVEFLAG_ONTRANSPORT        = 0x00000200 # ??
-    MOVEFLAG_LEVITATING = 0x00000400  # ?? Seems to be not working
-    MOVEFLAG_FIXED_Z = 0x00000800  # Fixed height. Jump => Scroll all over the map
-    MOVEFLAG_ROOT = 0x00001000
-    MOVEFLAG_JUMPING = 0x00002000
-    MOVEFLAG_FALLINGFAR = 0x00004000
-    MOVEFLAG_SWIMMING = 0x00200000  # Ok
-    MOVEFLAG_SPLINE_ENABLED = 0x00400000  # Ok
-    MOVEFLAG_CAN_FLY = 0x00800000
-    MOVEFLAG_FLYING = 0x01000000
-
-    MOVEFLAG_ONTRANSPORT = 0x02000000  # Used for flying on some creatures
-    MOVEFLAG_SPLINE_ELEVATION = 0x04000000  # Used for flight paths
-
-    MOVEFLAG_WATERWALKING = 0x10000000  # Prevent unit from falling through water
-    MOVEFLAG_SAFE_FALL = 0x20000000  # Active rogue safe fall spell (passive)
-    MOVEFLAG_HOVER = 0x40000000
-    MOVEFLAG_INTERNAL = 0x80000000
-
-    # Can not be present with MOVEFLAG_ROOT (otherwise client freezes)
-    MOVEFLAG_MASK_MOVING = MOVEFLAG_FORWARD | MOVEFLAG_BACKWARD | MOVEFLAG_STRAFE_LEFT | MOVEFLAG_STRAFE_RIGHT | MOVEFLAG_PITCH_UP | MOVEFLAG_PITCH_DOWN | MOVEFLAG_JUMPING | MOVEFLAG_FALLINGFAR | MOVEFLAG_SPLINE_ELEVATION
-    MOVEFLAG_MASK_MOVING_OR_TURN = MOVEFLAG_MASK_MOVING | MOVEFLAG_TURN_LEFT | MOVEFLAG_TURN_RIGHT
-
-    # MovementFlags mask that only contains flags for x/z translations
-    # this is to avoid that a jumping character that stands still triggers melee-leeway
-    MOVEFLAG_MASK_XZ = MOVEFLAG_FORWARD | MOVEFLAG_BACKWARD | MOVEFLAG_STRAFE_LEFT | MOVEFLAG_STRAFE_RIGHT
-
-
-# used in SMSG_MONSTER_MOVE
+# This is from 1.12, might be wrong
+# Used in SMSG_MONSTER_MOVE
 class SplineFlags(Enum):
     SPLINEFLAG_NONE = 0x00000000
     SPLINEFLAG_WALKMODE = 0x00000100
