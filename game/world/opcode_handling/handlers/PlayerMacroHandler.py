9c02960ccec63fbedcb42129f2be0896a0d19f24
GrenderG <grenderg@gmail.com>
2020-02-27 00:34:17 +0100

    Trying to implement CMSG_PLAYER_MACRO.

--- game/world/opcode_handling/handlers/PlayerMacroHandler.py
+++ game/world/opcode_handling/handlers/PlayerMacroHandler.py
@@ -7,9 +7,25 @@ class PlayerMacroHandler(object):
 
     @staticmethod
     def handle(world_session, socket, reader):
-        # TODO: Not implemented, not sure what to do with this data (called with /v oom, for example)
-        print('Size: %u' % len(reader.data))
-        print('Data: %s' % reader.data)
-        print('Data uint32: %u' % unpack('<I', reader.data))
+        # TODO: Not implemented, can't find a way to play sounds(called with /v oom, for example)
+        if len(reader.data) >= 4:
+            """
+            From SoundEntries.dbc, data received equals to:
+
+            00: Help Me
+            01: Incoming
+            02: Charge
+            03: Flee
+            04: Attack t...
+            05: Out of mana
+            06: Follow me
+            07: Wait here
+            08: Heal me
+            09: Cheer
+            10: Open fire
+            11: Raspberr...
+            """
+
+            socket.sendall(PacketWriter.get_packet(OpCode.SMSG_PLAYER_MACRO, pack('<I', reader.data)))
 
         return 0
