0127ac4a705f287685c31c072a0c54d24f97cd01
GrenderG <grenderg@gmail.com>
2020-03-02 00:22:31 +0100

    Taking only first 4 bytes when handling Stand State change opcode.

--- game/world/opcode_handling/handlers/StandStateChangeHandler.py
+++ game/world/opcode_handling/handlers/StandStateChangeHandler.py
@@ -10,7 +10,7 @@ class StandStateChangeHandler(object):
     @staticmethod
     def handle(world_session, socket, reader):
         if len(reader.data) >= 4:  # Avoid handling empty stand state packet
-            state = unpack('<I', reader.data)[0]
+            state = unpack('<I', reader.data[:4])[0]
             world_session.player_mgr.stand_state = StandState(state)
             world_session.player_mgr.flagged_for_update = True
 
