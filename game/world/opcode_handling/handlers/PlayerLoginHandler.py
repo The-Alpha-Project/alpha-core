a6bf0eabde505c7a6fe96de6ab927a120ce9d64f
GrenderG <grenderg@gmail.com>
2020-03-02 01:39:42 +0100

    Actually reverted it, lol.

--- game/world/opcode_handling/handlers/PlayerLoginHandler.py
+++ game/world/opcode_handling/handlers/PlayerLoginHandler.py
@@ -52,8 +52,8 @@ class PlayerLoginHandler(object):
             world_session.player_mgr.get_update_packet(update_type=UpdateTypes.UPDATE_FULL)))
         socket.sendall(update_packet)
 
-        time.sleep(0.5)
         PlayerLoginHandler._send_cinematic(world_session, world_session.player_mgr.player, socket)
+        time.sleep(0.5)
         world_session.player_mgr.complete_login()
 
         return 0
