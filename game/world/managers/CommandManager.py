28aaa928dc7b1c6af874b5c5ae6f75c893ab8815
Yushe <Yushe@users.noreply.github.com>
2020-04-08 19:35:50 +0100

    Update CommandManager.py

--- game/world/managers/CommandManager.py
+++ game/world/managers/CommandManager.py
@@ -73,7 +73,7 @@ class CommandManager(object):
             world_session.player_mgr.change_player_level(input_level)
             return 0, ''
         except ValueError:
-            return -1, 'Please specify a valid mount display id.'
+            return -1, 'Please specify a number.
 
     @staticmethod
     def gps(world_session, args):
