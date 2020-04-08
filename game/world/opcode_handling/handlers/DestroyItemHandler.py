a96e37110f8167d54de2f24b2fe1436b77877dda
GrenderG <grenderg@gmail.com>
2020-04-09 00:16:42 +0200

    Prevent destroying non-empty bags.

--- game/world/opcode_handling/handlers/DestroyItemHandler.py
+++ game/world/opcode_handling/handlers/DestroyItemHandler.py
@@ -2,7 +2,7 @@ from struct import pack, unpack
 
 from database.realm.RealmDatabaseManager import RealmDatabaseManager
 from network.packet.PacketWriter import *
-from utils.constants.ItemCodes import InventorySlots
+from utils.constants.ItemCodes import InventorySlots, InventoryError
 from utils.constants.ObjectCodes import UpdateTypes
 
 
@@ -17,14 +17,19 @@ class DestroyItemHandler(object):
 
             item = world_session.player_mgr.inventory.get_item(bag, source_slot)
             if item:
-                RealmDatabaseManager.character_inventory_delete(item.item_instance)
-                is_equipment = world_session.player_mgr.inventory.is_equipment_pos(bag, source_slot)
-                world_session.player_mgr.inventory.containers[bag].remove_item_in_slot(source_slot)
-                if is_equipment:
-                    world_session.player_mgr.flagged_for_update = True
+                if item.is_container() and not item.is_empty():
+                    world_session.player_mgr.inventory.send_equip_error(
+                        InventoryError.EQUIP_ERR_CAN_ONLY_DO_WITH_EMPTY_BAGS, item)
                 else:
-                    world_session.request.sendall(PacketWriter.get_packet(
-                        OpCode.SMSG_UPDATE_OBJECT,
-                        world_session.player_mgr.get_update_packet(update_type=UpdateTypes.UPDATE_FULL, is_self=True)))
+                    RealmDatabaseManager.character_inventory_delete(item.item_instance)
+                    is_equipment = world_session.player_mgr.inventory.is_equipment_pos(bag, source_slot)
+                    world_session.player_mgr.inventory.containers[bag].remove_item_in_slot(source_slot)
+                    if is_equipment:
+                        world_session.player_mgr.flagged_for_update = True
+                    else:
+                        world_session.request.sendall(PacketWriter.get_packet(
+                            OpCode.SMSG_UPDATE_OBJECT,
+                            world_session.player_mgr.get_update_packet(update_type=UpdateTypes.UPDATE_FULL,
+                                                                       is_self=True)))
 
         return 0
