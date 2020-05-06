8f7ad541d1417934d03d4b27a6ffd571ce730e82
GrenderG <grenderg@gmail.com>
2020-05-07 01:46:07 +0200

    Better handling of the hackfix for client sending wrong coordinates when jumping to the edge of an elevator in movement.

--- game/world/opcode_handling/handlers/MovementHandler.py
+++ game/world/opcode_handling/handlers/MovementHandler.py
@@ -19,9 +19,9 @@ class MovementHandler(object):
                 transport_guid, transport_x, transport_y, transport_z, transport_o, x, y, z, o, pitch, flags = \
                     unpack(movement_fmt, reader.data[:48])
 
-                # Prevent random teleports when colliding with elevators
-                if reader.opcode == OpCode.MSG_MOVE_COLLIDE_REDIRECT:
-                    if world_session.player_mgr.location.distance(Vector(x, y, z, o)) > 100:
+                # Hacky way to prevent random teleports when colliding with elevators
+                if world_session.player_mgr.transport_id != transport_guid:
+                    if world_session.player_mgr.location.distance(Vector(x, y, z, o)) > 64:
                         world_session.player_mgr.teleport(world_session.player_mgr.map_,
                                                           world_session.player_mgr.location)
 
