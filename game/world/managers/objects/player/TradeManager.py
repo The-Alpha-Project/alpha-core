dd524679faef5f4a5b99b98da3a6830fff89e454
GrenderG <grenderg@gmail.com>
2020-07-01 03:38:10 +0200

    Only send cancel trade status if there's an active trade.

--- game/world/managers/objects/player/TradeManager.py
+++ game/world/managers/objects/player/TradeManager.py
@@ -33,8 +33,8 @@ class TradeManager(object):
                                            TradeStatuses.TRADE_STATUS_CANCELLED)
             player.trade_data.other_player.trade_data = None
 
-        TradeManager.send_trade_status(player, TradeStatuses.TRADE_STATUS_CANCELLED)
-        player.trade_data = None
+            TradeManager.send_trade_status(player, TradeStatuses.TRADE_STATUS_CANCELLED)
+            player.trade_data = None
 
     @staticmethod
     def send_update_trade(player, trade_data, is_target):
