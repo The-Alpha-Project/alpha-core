2db6f2be97cf584988080fc70524ed811e795d3e
owniz <iam@jmoral.es>
2021-02-09 00:37:24 +0100

    Added queries to know previous rank spell

---
+++ etc/databases/dbc/updates/updates.sql
@@ -0,0 +1,16 @@
+delimiter $
+begin not atomic
+    -- 09/02/2021 1
+    if (select count(*) from applied_updates where id='090220211') = 0 then
+        CREATE TABLE `applied_updates` (`id` varchar(9) NOT NULL DEFAULT '000000000', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        
+        alter table SkillLineAbility add column custom_PrecededBySpell int(11) not null default 0;
+        
+        UPDATE SkillLineAbility t1
+        INNER JOIN SkillLineAbility t2 ON t2.SupercededBySpell = t1.Spell
+        SET t1.custom_PrecededBySpell = t2.Spell;
+
+        insert into applied_updates values ('090220211');
+    end if;
+end $
+delimiter ;
