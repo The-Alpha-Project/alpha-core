c8834e8785aa17a07729cb1575b427b6d4042667
GrenderG <grenderg@gmail.com>
2021-02-09 02:32:02 +0100

    Fix dbc sql update file.

--- etc/databases/dbc/updates/updates.sql
+++ etc/databases/dbc/updates/updates.sql
@@ -1,9 +1,9 @@
+CREATE TABLE IF NOT EXISTS `applied_updates` (`id` varchar(9) NOT NULL DEFAULT '000000000', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 delimiter $
 begin not atomic
     -- 09/02/2021 1
     if (select count(*) from applied_updates where id='090220211') = 0 then
-        CREATE TABLE `applied_updates` (`id` varchar(9) NOT NULL DEFAULT '000000000', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-        
         alter table SkillLineAbility add column custom_PrecededBySpell int(11) not null default 0;
         
         UPDATE SkillLineAbility t1
