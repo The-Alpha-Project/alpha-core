a16aa460b3a175e7f894f13a7297e1340a3ae99c
GrenderG <grenderg@gmail.com>
2021-02-11 19:01:05 +0100

    Many gobject fixes / improvements.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -6,6 +6,59 @@ begin not atomic
         update spawns_creatures set ignored = 1 where spawn_entry1 in (10665, 10666);
         update quest_template set ignored = 1 where entry in (5481, 5482);
 
+        /* Main Hall Chair */
+        update spawns_gameobjects set spawn_entry = 37069 where spawn_id = 44837;
+        update spawns_gameobjects set spawn_entry = 37066 where spawn_id = 44838;
+        update spawns_gameobjects set spawn_entry = 37057 where spawn_id = 44826;
+        update spawns_gameobjects set spawn_entry = 37058 where spawn_id = 44832;
+        update spawns_gameobjects set spawn_entry = 37059 where spawn_id = 44830;
+        update spawns_gameobjects set spawn_entry = 37062 where spawn_id = 44820;
+        update spawns_gameobjects set spawn_entry = 37063 where spawn_id = 44819;
+        update spawns_gameobjects set spawn_entry = 37064 where spawn_id = 44817;
+        update spawns_gameobjects set spawn_entry = 37065 where spawn_id = 44823;
+        update spawns_gameobjects set spawn_entry = 37067 where spawn_id = 44824;
+        update spawns_gameobjects set spawn_entry = 37068 where spawn_id = 44825;
+        update spawns_gameobjects set spawn_entry = 37060 where spawn_id = 44818;
+        update spawns_gameobjects set spawn_entry = 37061 where spawn_id = 44828;
+
+        /* Elegant Chair */
+        replace into spawns_gameobjects (spawn_entry, spawn_positionX, spawn_positionY, spawn_positionZ, spawn_orientation) values
+        (37070, 2302.332734, 265.124669, 38.669846, 1.317196);
+
+        /* Old Pew */
+        replace into spawns_gameobjects (spawn_entry, spawn_positionX, spawn_positionY, spawn_positionZ, spawn_orientation) values
+        (37053, 2309.762451, 276.017761, 37.310703, 4.458355),
+        (37054, 2308.682861, 272.583588, 37.310661, 4.426939),
+
+        (37052, 2306.001465, 270.191498, 37.310383, 1.312838),
+
+        (37055, 2317.793359, 279.477142, 37.310703, 2.869500),
+        (37056, 2305.555420, 282.496826, 37.310570, 6.015017);
+
+        update spawns_gameobjects set spawn_positionX = 2263.089150, spawn_positionY = 249.919006, spawn_positionZ = 41.114769,
+                                      spawn_orientation = 6.084185, spawn_rotation2 = 0, spawn_rotation3 = 0 where spawn_id = 44866;
+
+        update spawns_gameobjects set spawn_positionX = -9459.416992, spawn_positionY = 20.861105, spawn_positionZ = 63.820412,
+                                      spawn_orientation = 4.596528, spawn_rotation2 = 0, spawn_rotation3 = 0, ignored = 0 where spawn_id = 26243;
+
+        update spawns_gameobjects set ignored = 1 where spawn_id in (44827, 44829, 44834, 44822, 44836, 44833,
+                                                                    44839, 44821, 44835, 44831);
+
+        alter table gameobject_template drop column data10;
+        alter table gameobject_template drop column data11;
+        alter table gameobject_template drop column data12;
+        alter table gameobject_template drop column data13;
+        alter table gameobject_template drop column data14;
+        alter table gameobject_template drop column data15;
+        alter table gameobject_template drop column data16;
+        alter table gameobject_template drop column data17;
+        alter table gameobject_template drop column data18;
+        alter table gameobject_template drop column data19;
+        alter table gameobject_template drop column data20;
+        alter table gameobject_template drop column data21;
+        alter table gameobject_template drop column data22;
+        alter table gameobject_template drop column data23;
+
         insert into applied_updates values ('110220211');
     end if;
 end $
