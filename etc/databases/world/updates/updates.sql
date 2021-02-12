875d3a63a3a25ba033dfd3b91c697d09904358c5
GrenderG <grenderg@gmail.com>
2021-02-12 01:24:44 +0100

    Typo in db update.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -73,8 +73,8 @@ begin not atomic
         insert into applied_updates values ('110220211');
     end if;
 
-    -- 11/02/2021 1
-    if (select count(*) from applied_updates where id='110220211') = 0 then
+    -- 12/02/2021 1
+    if (select count(*) from applied_updates where id='120220211') = 0 then
         update player_classlevelstats set basemana = 0 where class = 3;
     end if;
 end $
