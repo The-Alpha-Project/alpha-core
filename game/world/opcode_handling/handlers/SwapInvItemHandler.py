37456fd11f0bae5e3b970b2f3fc7d865ef2de12e
GrenderG <grenderg@gmail.com>
2021-02-21 21:44:04 +0100

    Remove already done TODO.

--- game/world/opcode_handling/handlers/SwapInvItemHandler.py
+++ game/world/opcode_handling/handlers/SwapInvItemHandler.py
@@ -11,6 +11,5 @@ class SwapInvItemHandler(object):
         if len(reader.data) >= 2:  # Avoid handling empty swap inv item packet
             source_slot, dest_slot = unpack('<2B', reader.data[:2])
             bag = InventorySlots.SLOT_INBACKPACK.value
-            # TODO: Handle item equip action
             world_session.player_mgr.inventory.swap_item(bag, source_slot, bag, dest_slot)
         return 0
