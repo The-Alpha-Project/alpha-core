d7595174c45e38da9aa1d55f44e53fd6dedfe245
GrenderG <grenderg@gmail.com>
2021-03-06 23:14:24 +0100

    Reduce base detection range. https://github.com/vmangos/core/commit/cf487801e7a6f5f297185e3e17902e835a9cc15b.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -132,5 +132,17 @@ begin not atomic
 
         insert into applied_updates values ('010320211');
     end if;
+
+    -- 06/03/2021 1
+    if (select count(*) from applied_updates where id='060320211') = 0 then
+        -- https://github.com/vmangos/core/commit/cf487801e7a6f5f297185e3e17902e835a9cc15b
+        -- Aggro range for mobs in starting zones is lower than normal.
+        UPDATE `creature_template` SET `detection_range`=8 WHERE `entry` IN (6, 38, 69, 80, 103, 257, 299, 704, 705, 706, 707, 708, 724, 808, 946, 1501, 1502, 1504, 1505, 1506, 1507, 1508, 1509, 1512, 1513, 1667, 1688, 1890, 1916, 1917, 1918, 1919, 1984, 1985, 1986, 1988, 1989, 1994, 2031, 2032, 2952, 2953, 2954, 2955, 2961, 2966, 3098, 3101, 3102, 3124, 3183, 3229, 3281, 3300, 8554);
+
+        -- Aggro range for most mobs in classic is around 3-4 yards lower than our current default.
+        UPDATE `creature_template` SET `detection_range`=16 WHERE `detection_range`=20 && `rank`=0;
+
+        insert into applied_updates values ('060320211');
+    end if;
 end $
 delimiter ;
