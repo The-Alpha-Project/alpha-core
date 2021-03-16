d51ecd4f64b521ed37f62eaf60bbea186f11448c
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-03-15 18:52:00 -0600

    * Fix crash.

--- game/world/managers/objects/loot/LootManager.py
+++ game/world/managers/objects/loot/LootManager.py
@@ -13,25 +13,34 @@ class LootManager(object):
             money = randint(creature.creature_template.gold_min, creature.creature_template.gold_max)
             creature.money = money
 
-            _creature_loot = []
+            _item_templates = []
             for lt in LootManager.CREATURE_LOOT_TEMPLATES:
                 if lt.entry == creature.entry:
-                    _creature_loot.append(lt)
+                    item_template = WorldDatabaseManager.item_template_get_by_entry(lt.item)
+                    if item_template:
+                        _item_templates.append(lt)
 
-            if _creature_loot:
+            if _item_templates:
                 chance = float(round(uniform(0.0, 1.0), 2) * 100)
-                for loot_temp in _creature_loot:
+                for loot_temp in _item_templates:
                     item_chance = loot_temp.ChanceOrQuestChance
                     item_chance = item_chance if item_chance > 0 else item_chance * -1
 
+                    item = None
                     if item_chance >= 100:
-                        creature.loot.append(loot_temp.item)
-                        break
+                        item = ItemManager.generate_item_from_entry(loot_temp.item)
 
                     chance -= item_chance
                     if chance <= 0:
                         item = ItemManager.generate_item_from_entry(loot_temp.item)
 
-                        if item:
-                            creature.loot.append(item)
-                            break
+                    if item:
+                        print(f'Item: entry {item.entry} dID {item.display_id} name {item.item_template.name}')
+                        creature.loot.append(item)
+                        break
+
+        print('[DEBUG] Loot generated:')
+        print(f"[DEBUG]\tMoney: {creature.money}")
+        print('[DEBUG] Items:')
+        for item in creature.loot:
+            print(f'[DEBUG]\t{item.item_template.name}')
