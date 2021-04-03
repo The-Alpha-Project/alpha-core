8a080f6145bf4333c342da2c54ee4e7f68da6dd8
Fluglow <Jaakko.bonifer@gmail.com>
2021-04-03 18:42:33 +0300

    Simplify DestroyItemHandler

--- game/world/opcode_handling/handlers/inventory/DestroyItemHandler.py
+++ game/world/opcode_handling/handlers/inventory/DestroyItemHandler.py
@@ -16,25 +16,18 @@ class DestroyItemHandler(object):
                 bag = InventorySlots.SLOT_INBACKPACK.value
 
             item = world_session.player_mgr.inventory.get_item(bag, source_slot)
-            if item:
-                if item.is_container() and not item.is_empty():
-                    world_session.player_mgr.inventory.send_equip_error(
-                        InventoryError.BAG_NOT_EMPTY, item)
-                else:
-                    RealmDatabaseManager.character_inventory_delete(item.item_instance)
-                    world_session.player_mgr.inventory.mark_as_removed(item)
-                    world_session.player_mgr.session.request.sendall(item.get_destroy_packet())
+            if not item:
+                return 0
 
-                    if world_session.player_mgr.inventory.is_bag_pos(source_slot):
-                        world_session.player_mgr.inventory.remove_bag(source_slot)
-                    else:
-                        container = world_session.player_mgr.inventory.get_container(bag)
-                        if container:
-                            container.remove_item_in_slot(source_slot)
+            if item.is_container() and not item.is_empty():
+                world_session.player_mgr.inventory.send_equip_error(
+                    InventoryError.BAG_NOT_EMPTY, item)
+                return 0
 
-                    if world_session.player_mgr.inventory.is_equipment_pos(bag, source_slot):
-                        world_session.player_mgr.set_dirty(dirty_inventory=True)
-                    else:
-                        world_session.player_mgr.send_update_self()
+            world_session.player_mgr.inventory.remove_item(bag, source_slot, True)
 
+            if world_session.player_mgr.inventory.is_equipment_pos(bag, source_slot):
+                world_session.player_mgr.set_dirty(dirty_inventory=True)
+            else:
+                world_session.player_mgr.send_update_self()
         return 0
