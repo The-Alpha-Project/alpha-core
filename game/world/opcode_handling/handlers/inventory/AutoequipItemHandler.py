d3becc4b82b5f5b37f16a121c8426ad5922a4be6
Fluglow <Jaakko.bonifer@gmail.com>
2021-04-03 18:07:52 +0300

    Restructure InventoryManager, implement remove_item, implement offhand removal, fix Autoequip error

--- game/world/opcode_handling/handlers/inventory/AutoequipItemHandler.py
+++ game/world/opcode_handling/handlers/inventory/AutoequipItemHandler.py
@@ -15,30 +15,24 @@ class AutoequipItemHandler(object):
             inventory = world_session.player_mgr.inventory
             source_container = inventory.get_container(source_bag_slot)
             source_item = inventory.get_item(source_bag_slot, source_slot)
-            target_bag_slot = InventorySlots.SLOT_INBACKPACK.value
 
-            if source_container and source_item:
-                inv_type = source_item.item_template.inventory_type
-                if inv_type == InventoryTypes.BAG:
-                    target_slot = inventory.get_next_available_bag_slot().value
-                else:
-                    # If a main weapon already exists, equip this one in the offhand unless there's already one
-                    if world_session.player_mgr.skill_manager.can_dual_wield() \
-                            and source_item.item_template.inventory_type == InventoryTypes.WEAPON \
-                            and inventory.has_main_weapon() and not inventory.has_offhand():
-                        target_slot = InventorySlots.SLOT_OFFHAND.value
-                        source_item.equip_slot = InventorySlots.SLOT_OFFHAND.value
-                    else:
-                        target_slot = source_item.equip_slot
-            else:
+            if not source_container or not source_item:
                 # Something is really malfunctioning or client sent wrong data, try to reload the inventory
                 world_session.player_mgr.set_dirty(is_dirty=True, dirty_inventory=True)
                 return 0
 
-            if target_slot == -1:
-                # TODO Irrelevant error code, but only one that seems to not display anything to the client.
-                inventory.send_equip_error(InventoryError.BAG_LOOT_GONE, source_item, None)
+            inv_type = source_item.item_template.inventory_type
+            target_slot = inventory.get_next_available_slot_for_inv_type(inv_type)
+
+            # Equipment (excluding bags) can be swapped in with autoequip.
+            # If there's no available slot (target_slot == -1), swap into default equip slot
+            if target_slot == -1 and inv_type != InventoryTypes.NONE_EQUIP and inv_type != InventoryTypes.BAG:
+                target_slot = source_item.equip_slot
+
+            target_bag_slot = InventorySlots.SLOT_INBACKPACK.value
+
+            if target_slot == -1 or inv_type == InventoryTypes.NONE_EQUIP:  # unequippable item or no free bag slot for bag autoequip
+                inventory.send_equip_error(InventoryError.BAG_NOT_EQUIPPABLE, source_item, None)
                 return 0
             inventory.swap_item(source_bag_slot, source_slot, target_bag_slot, target_slot)
-
         return 0
