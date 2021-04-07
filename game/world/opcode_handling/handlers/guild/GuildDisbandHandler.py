ec284f5cb5248d38dec463fcbda1dac56f9bfb85
GrenderG <grenderg@gmail.com>
2021-04-07 20:35:35 +0200

    Small refactor.

--- game/world/opcode_handling/handlers/guild/GuildDisbandHandler.py
+++ game/world/opcode_handling/handlers/guild/GuildDisbandHandler.py
@@ -12,7 +12,8 @@ class GuildDisbandHandler(object):
             GuildManager.send_guild_command_result(player_mgr, GuildTypeCommand.GUILD_CREATE_S, '',
                                                    GuildCommandResults.GUILD_PLAYER_NOT_IN_GUILD)
         elif player_mgr != player_mgr.guild_manager.guild_master:
-            GuildManager.send_guild_command_result(player_mgr, GuildTypeCommand.GUILD_FOUNDER_S, '', GuildCommandResults.GUILD_PERMISSIONS)
+            GuildManager.send_guild_command_result(player_mgr, GuildTypeCommand.GUILD_FOUNDER_S, '',
+                                                   GuildCommandResults.GUILD_PERMISSIONS)
         else:
             player_mgr.guild_manager.disband()
 
