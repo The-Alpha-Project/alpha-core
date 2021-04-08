39c79257cf6bbdef13bfd721a2eb9806b0c500f7
GrenderG <grenderg@gmail.com>
2021-04-08 20:21:48 +0200

    Fix logic in GuildMOTDHandler.

--- game/world/opcode_handling/handlers/guild/GuildMOTDHandler.py
+++ game/world/opcode_handling/handlers/guild/GuildMOTDHandler.py
@@ -10,15 +10,12 @@ class GuildMOTDHandler(object):
         motd = PacketReader.read_string(reader.data, 0).strip()
         player_mgr = world_session.player_mgr
 
-        if not motd:
-            player_mgr.guild_manager.send_motd()
-            return 0
-
         if not player_mgr.guild_manager:
             GuildManager.send_guild_command_result(player_mgr, GuildTypeCommand.GUILD_INVITE_S, '',
                                                    GuildCommandResults.GUILD_PLAYER_NOT_IN_GUILD)
-        # TODO Maybe officer could also set MOTD
-        elif player_mgr != player_mgr.guild_manager.guild_master:
+        elif not motd:
+            player_mgr.guild_manager.send_motd()
+        elif player_mgr != player_mgr.guild_manager.guild_master:  # TODO Maybe officer could also set MOTD
             GuildManager.send_guild_command_result(player_mgr, GuildTypeCommand.GUILD_INVITE_S, '',
                                                    GuildCommandResults.GUILD_PERMISSIONS)
         else:
