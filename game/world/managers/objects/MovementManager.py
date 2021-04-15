ea4f5dc832929f3024bb1f938c8aa916b1dc849d
GrenderG <grenderg@gmail.com>
2021-04-15 23:55:43 +0200

    Add 1 second delay on dismount after reaching taxi destination.

--- game/world/managers/objects/MovementManager.py
+++ game/world/managers/objects/MovementManager.py
@@ -127,7 +127,11 @@ class MovementManager(object):
         GridManager.send_surrounding(PacketWriter.get_packet(OpCode.SMSG_MONSTER_MOVE, data), self.unit,
                                      include_self=self.is_player)
 
-        self.total_waypoint_time = total_time
+        # Player shouldn't instantly dismount after reaching the taxi destination
+        if self.is_player and spline_flag == SplineFlags.SPLINEFLAG_FLYING:
+            self.total_waypoint_time = total_time + 1.0  # Add 1 extra second
+        else:
+            self.total_waypoint_time = total_time
 
         # Generate the spline
         spline = MovementSpline()
