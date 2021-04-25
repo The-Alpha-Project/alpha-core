ccbef7107d7f74afe97efe063ae5a88d9983fbe6
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-04-24 19:16:15 -0500

    * Fix money loot bug.
    / Money 'sound' should no longer trigger twice making you feel rich.

--- game/world/managers/objects/player/PlayerManager.py
+++ game/world/managers/objects/player/PlayerManager.py
@@ -477,10 +477,8 @@ class PlayerManager(UnitManager):
             if enemy and enemy.loot_manager.has_money():
                 if self.group_manager and self.group_manager.is_party_formed():
                     self.group_manager.reward_group_money(self, enemy)
-                else:
+                else: # No party, you don't get 'Your share of the loot was' since you are looting all of it.
                     self.session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_LOOT_CLEAR_MONEY))
-                    data = pack('<I', enemy.loot_manager.current_money)
-                    self.session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_LOOT_MONEY_NOTIFY, data))
                     self.mod_money(enemy.loot_manager.current_money)
                     enemy.loot_manager.clear_money()
 
