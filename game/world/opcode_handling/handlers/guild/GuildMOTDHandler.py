069728d5c9bb78ffa7a4d3e172319ac2826560b5
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-05-03 19:26:52 -0500

    * Moved Grender workaround to retrieve motd.

--- game/world/opcode_handling/handlers/guild/GuildMOTDHandler.py
+++ game/world/opcode_handling/handlers/guild/GuildMOTDHandler.py
@@ -14,12 +14,12 @@ class GuildMOTDHandler(object):
             if not player_mgr.guild_manager:
                 GuildManager.send_guild_command_result(player_mgr, GuildTypeCommand.GUILD_INVITE_S, '',
                                                        GuildCommandResults.GUILD_PLAYER_NOT_IN_GUILD)
-            elif not motd:
-                player_mgr.guild_manager.send_motd()
             elif player_mgr.guild_manager.get_rank(player_mgr.guid) > GuildRank.GUILDRANK_OFFICER:
                 GuildManager.send_guild_command_result(player_mgr, GuildTypeCommand.GUILD_INVITE_S, '',
                                                        GuildCommandResults.GUILD_PERMISSIONS)
             else:
                 player_mgr.guild_manager.set_motd(motd)
+        elif world_session.player_mgr.guild_manager:
+            world_session.player_mgr.guild_manager.send_motd()
 
         return 0
