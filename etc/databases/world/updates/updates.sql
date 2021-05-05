770872b51b6c27ed284302d0f1625ee4dc4d68c8
GrenderG <grenderg@gmail.com>
2021-05-06 01:44:23 +0200

    Quest handling improvements + added completion text for quest 787.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -6,6 +6,12 @@ begin not atomic
         update item_template set display_id = 3865 where entry = 7725;
         insert into applied_updates values ('050520211');
     end if;
+
+    -- 06/05/2021 1
+    if (select count(*) from applied_updates where id='060520211') = 0 then
+        update quest_template set OfferRewardText = 'Another one of Eitrigg''s recruits, hm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you''re ready to leave the Valley, you''ll be a proud warrior of the Horde.', RewXP = 15 where entry = 787;
+        insert into applied_updates values ('060520211');
+    end if;
 	
 end $
 delimiter ;
--- game/world/opcode_handling/handlers/quest/QuestGiverQueryQuestHandler.py
+++ game/world/opcode_handling/handlers/quest/QuestGiverQueryQuestHandler.py
@@ -6,6 +6,7 @@ from utils.Logger import Logger
 from utils.constants import ObjectCodes
 
 from network.packet.PacketWriter import PacketWriter, OpCode
+from utils.constants.ObjectCodes import HighGuid
 
 
 class QuestGiverQueryQuestHandler(object):
@@ -14,15 +15,33 @@ class QuestGiverQueryQuestHandler(object):
     def handle(world_session, socket, reader):
         if len(reader.data) >= 8:  # Avoid handling empty quest giver query quest packet
             guid, quest_entry = unpack('<QL', reader.data[:12])
-            # Quest giver is an NPC or an item
-            quest_giver = GridManager.get_surrounding_unit_by_guid(world_session.player_mgr, guid) or world_session.player_mgr.inventory.get_item_info_by_guid(guid)[3].item_template
-            if not quest_giver:
-                Logger.error(f'Error in CMSG_QUESTGIVER_QUERY_QUEST, could not find quest giver with guid of: {guid}')
-                return 0
+            # NPC
+            if guid & HighGuid.HIGHGUID_UNIT:
+                quest_giver = GridManager.get_surrounding_unit_by_guid(world_session.player_mgr, guid)
+                if not quest_giver:
+                    return 0
 
-            quest_giver_is_related = world_session.player_mgr.quest_manager.check_quest_giver_npc_is_related(quest_giver.entry, quest_entry) or quest_giver.start_quest == quest_entry
-            if not quest_giver_is_related:
-                Logger.error(f'Error in CMSG_QUESTGIVER_QUERY_QUEST, quest giver {quest_giver.entry} was not related to quest {quest_entry}')
+                quest_giver_is_related = world_session.player_mgr.quest_manager.check_quest_giver_npc_is_related(
+                    quest_giver.entry, quest_entry)
+                if not quest_giver_is_related:
+                    return 0
+            # Gameobject
+            elif guid & HighGuid.HIGHGUID_GAMEOBJECT:
+                quest_giver = GridManager.get_surrounding_gameobject_by_guid(world_session, guid)
+                if not quest_giver:
+                    return 0
+            # Item
+            elif guid & HighGuid.HIGHGUID_ITEM:
+                item_info = world_session.player_mgr.inventory.get_item_info_by_guid(guid)
+                if not item_info[3]:
+                    return 0
+
+                quest_giver = item_info[3].item_template
+                quest_giver_is_related = quest_giver.start_quest == quest_entry
+                if not quest_giver_is_related:
+                    return 0
+            else:
+                Logger.error(f'Error in CMSG_QUESTGIVER_QUERY_QUEST, unknown quest giver type.')
                 return 0
 
             quest = WorldDatabaseManager.QuestTemplateHolder.quest_get_by_entry(quest_entry)
