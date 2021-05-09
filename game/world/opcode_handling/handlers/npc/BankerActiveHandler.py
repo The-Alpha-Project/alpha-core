a161164d79e66f0b8bd05ab179052ee2d13526f3
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-05-09 13:55:37 -0500

    Update BankerActiveHandler.py

--- game/world/opcode_handling/handlers/npc/BankerActiveHandler.py
+++ game/world/opcode_handling/handlers/npc/BankerActiveHandler.py
@@ -11,7 +11,7 @@ class BankerActiveHandler(object):
         if len(reader.data) >= 8:  # Avoid handling empty guid
             guid = unpack('<Q', reader.data[:8])[0]
             npc = MapManager.get_surrounding_unit_by_guid(world_session.player_mgr, guid)
-            if guid > 0 and npc:
+            if npc:
                 data = pack('<Q', guid)
                 world_session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_SHOW_BANK, data))
         return 0
