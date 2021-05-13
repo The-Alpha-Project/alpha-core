7c563af66c5a5753498f038a78eb5c8cdaac2d21
GrenderG <grenderg@gmail.com>
2021-05-13 22:28:21 +0200

    New world and realm database releases.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1,30 +1,12 @@
 delimiter $
 begin not atomic
-    -- 05/05/2021 1
-    if (select count(*) from applied_updates where id='050520211') = 0 then
-        delete from item_template where entry = 23192;
-        update item_template set display_id = 3865 where entry = 7725;
-        insert into applied_updates values ('050520211');
-    end if;
+    -- 13/05/2021 2
+    if (select count(*) from applied_updates where id='130520212') = 0 then
+        -- Fix damage and armor of starting mobs.
+        UPDATE `creature_template` SET `dmg_min` = 1, `dmg_max` = 2, `armor` = 16 WHERE `entry` IN (6, 707, 1512, 2955, 3098);
+        UPDATE `creature_template` SET `dmg_min` = 1, `dmg_max` = 2, `armor` = 15 WHERE `entry` = 1501;
 
-    -- 06/05/2021 1
-    if (select count(*) from applied_updates where id='060520211') = 0 then
-        update quest_template set OfferRewardText = 'Another one of Eitrigg''s recruits, hm?$B$BA sorry state of affairs we find ourselves in if this is the best the Horde can produce. No matter. By the time we think you''re ready to leave the Valley, you''ll be a proud warrior of the Horde.', RewXP = 15 where entry = 787;
-        insert into applied_updates values ('060520211');
-    end if;
-
-    -- 11/05/2021 1
-    if (select count(*) from applied_updates where id='110520211') = 0 then
-        UPDATE `creature_template` set `npc_flags` = 16388, `level_min` = 30, `level_max` = 30 WHERE `entry` = 5814;
-        DELETE FROM `npc_vendor` WHERE `entry` = 5814;
-        INSERT INTO `npc_vendor` (`entry`, `item`) VALUES (5814, 2511);
-        insert into applied_updates values ('110520211');
-    end if;
-
-    -- 11/05/2021 2
-    if (select count(*) from applied_updates where id='110520212') = 0 then
-        delete from spawns_gameobjects where spawn_id = 49853;
-        insert into applied_updates values ('110520212');
+        insert into applied_updates values ('130520212');
     end if;
 	
 end $
