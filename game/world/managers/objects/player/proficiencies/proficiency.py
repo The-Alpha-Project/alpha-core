11f2253ce90b7d4ea146c8e6c4f5f83fa9d91293
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-05-15 22:24:53 -0500

    Update proficiency.py

--- game/world/managers/objects/player/proficiencies/proficiency.py
+++ game/world/managers/objects/player/proficiencies/proficiency.py
@@ -7,135 +7,104 @@ class Proficiency(object):
 
     @staticmethod
     def build_from_chr_proficiency(chr_proficiency):
-        proficiencies = []
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_1,
-                        chr_proficiency.Proficiency_AcquireMethod_1,
-                        chr_proficiency.Proficiency_ItemClass_1,
-                        chr_proficiency.Proficiency_ItemSubClassMask_1,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_2,
-                        chr_proficiency.Proficiency_AcquireMethod_2,
-                        chr_proficiency.Proficiency_ItemClass_2,
-                        chr_proficiency.Proficiency_ItemSubClassMask_2,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_3,
-                        chr_proficiency.Proficiency_AcquireMethod_3,
-                        chr_proficiency.Proficiency_ItemClass_3,
-                        chr_proficiency.Proficiency_ItemSubClassMask_3,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_4,
-                        chr_proficiency.Proficiency_AcquireMethod_4,
-                        chr_proficiency.Proficiency_ItemClass_4,
-                        chr_proficiency.Proficiency_ItemSubClassMask_4,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_5,
-                        chr_proficiency.Proficiency_AcquireMethod_5,
-                        chr_proficiency.Proficiency_ItemClass_5,
-                        chr_proficiency.Proficiency_ItemSubClassMask_5,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_6,
-                        chr_proficiency.Proficiency_AcquireMethod_6,
-                        chr_proficiency.Proficiency_ItemClass_6,
-                        chr_proficiency.Proficiency_ItemSubClassMask_6,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_7,
-                        chr_proficiency.Proficiency_AcquireMethod_7,
-                        chr_proficiency.Proficiency_ItemClass_7,
-                        chr_proficiency.Proficiency_ItemSubClassMask_7,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_8,
-                        chr_proficiency.Proficiency_AcquireMethod_8,
-                        chr_proficiency.Proficiency_ItemClass_8,
-                        chr_proficiency.Proficiency_ItemSubClassMask_8,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_9,
-                        chr_proficiency.Proficiency_AcquireMethod_9,
-                        chr_proficiency.Proficiency_ItemClass_9,
-                        chr_proficiency.Proficiency_ItemSubClassMask_9,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_10,
-                        chr_proficiency.Proficiency_AcquireMethod_10,
-                        chr_proficiency.Proficiency_ItemClass_10,
-                        chr_proficiency.Proficiency_ItemSubClassMask_10,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_11,
-                        chr_proficiency.Proficiency_AcquireMethod_11,
-                        chr_proficiency.Proficiency_ItemClass_11,
-                        chr_proficiency.Proficiency_ItemSubClassMask_11,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_12,
-                        chr_proficiency.Proficiency_AcquireMethod_12,
-                        chr_proficiency.Proficiency_ItemClass_12,
-                        chr_proficiency.Proficiency_ItemSubClassMask_12,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_13,
-                        chr_proficiency.Proficiency_AcquireMethod_13,
-                        chr_proficiency.Proficiency_ItemClass_13,
-                        chr_proficiency.Proficiency_ItemSubClassMask_13,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_14,
-                        chr_proficiency.Proficiency_AcquireMethod_14,
-                        chr_proficiency.Proficiency_ItemClass_14,
-                        chr_proficiency.Proficiency_ItemSubClassMask_14,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_15,
-                        chr_proficiency.Proficiency_AcquireMethod_15,
-                        chr_proficiency.Proficiency_ItemClass_15,
-                        chr_proficiency.Proficiency_ItemSubClassMask_15,
-                        )
-        )
-
-        proficiencies.append(
-            Proficiency(chr_proficiency.Proficiency_MinLevel_16,
-                        chr_proficiency.Proficiency_AcquireMethod_16,
-                        chr_proficiency.Proficiency_ItemClass_16,
-                        chr_proficiency.Proficiency_ItemSubClassMask_16,
-                        )
-        )
+        proficiencies = [
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_1,
+                chr_proficiency.Proficiency_AcquireMethod_1,
+                chr_proficiency.Proficiency_ItemClass_1,
+                chr_proficiency.Proficiency_ItemSubClassMask_1
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_2,
+                chr_proficiency.Proficiency_AcquireMethod_2,
+                chr_proficiency.Proficiency_ItemClass_2,
+                chr_proficiency.Proficiency_ItemSubClassMask_2
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_3,
+                chr_proficiency.Proficiency_AcquireMethod_3,
+                chr_proficiency.Proficiency_ItemClass_3,
+                chr_proficiency.Proficiency_ItemSubClassMask_3
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_4,
+                chr_proficiency.Proficiency_AcquireMethod_4,
+                chr_proficiency.Proficiency_ItemClass_4,
+                chr_proficiency.Proficiency_ItemSubClassMask_4
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_5,
+                chr_proficiency.Proficiency_AcquireMethod_5,
+                chr_proficiency.Proficiency_ItemClass_5,
+                chr_proficiency.Proficiency_ItemSubClassMask_5
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_6,
+                chr_proficiency.Proficiency_AcquireMethod_6,
+                chr_proficiency.Proficiency_ItemClass_6,
+                chr_proficiency.Proficiency_ItemSubClassMask_6
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_7,
+                chr_proficiency.Proficiency_AcquireMethod_7,
+                chr_proficiency.Proficiency_ItemClass_7,
+                chr_proficiency.Proficiency_ItemSubClassMask_7
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_8,
+                chr_proficiency.Proficiency_AcquireMethod_8,
+                chr_proficiency.Proficiency_ItemClass_8,
+                chr_proficiency.Proficiency_ItemSubClassMask_8
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_9,
+                chr_proficiency.Proficiency_AcquireMethod_9,
+                chr_proficiency.Proficiency_ItemClass_9,
+                chr_proficiency.Proficiency_ItemSubClassMask_9
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_10,
+                chr_proficiency.Proficiency_AcquireMethod_10,
+                chr_proficiency.Proficiency_ItemClass_10,
+                chr_proficiency.Proficiency_ItemSubClassMask_10
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_11,
+                chr_proficiency.Proficiency_AcquireMethod_11,
+                chr_proficiency.Proficiency_ItemClass_11,
+                chr_proficiency.Proficiency_ItemSubClassMask_11
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_12,
+                chr_proficiency.Proficiency_AcquireMethod_12,
+                chr_proficiency.Proficiency_ItemClass_12,
+                chr_proficiency.Proficiency_ItemSubClassMask_12
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_13,
+                chr_proficiency.Proficiency_AcquireMethod_13,
+                chr_proficiency.Proficiency_ItemClass_13,
+                chr_proficiency.Proficiency_ItemSubClassMask_13
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_14,
+                chr_proficiency.Proficiency_AcquireMethod_14,
+                chr_proficiency.Proficiency_ItemClass_14,
+                chr_proficiency.Proficiency_ItemSubClassMask_14
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_15,
+                chr_proficiency.Proficiency_AcquireMethod_15,
+                chr_proficiency.Proficiency_ItemClass_15,
+                chr_proficiency.Proficiency_ItemSubClassMask_15
+                ),
+            Proficiency(
+                chr_proficiency.Proficiency_MinLevel_16,
+                chr_proficiency.Proficiency_AcquireMethod_16,
+                chr_proficiency.Proficiency_ItemClass_16,
+                chr_proficiency.Proficiency_ItemSubClassMask_16
+                )
+        ]
 
         return proficiencies
 
