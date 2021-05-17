49e865072e7686ad165e1c983d3c9c4c3b3ff127
GrenderG <grenderg@gmail.com>
2021-05-17 14:28:10 +0200

    Fixt sql statement.

--- etc/databases/realm/updates/updates.sql
+++ etc/databases/realm/updates/updates.sql
@@ -17,7 +17,9 @@ begin not atomic
 
     -- 17/05/2021 1
 	if (select count(*) from applied_updates where id='170520211') = 0 then
+        SET FOREIGN_KEY_CHECKS = 0;
 		alter table petition change petition_guid item_guid int(11) unsigned not null default 0;
+        SET FOREIGN_KEY_CHECKS = 1;
 
 		insert into applied_updates values ('170520211');
     end if;
