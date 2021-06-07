932d5aecdceeebf100af2ee0279901841fa7e47c
radongc <ccostanzo115@gmail.com>
2021-06-06 20:15:48 -0400

    More opcode changes

--- game/world/opcode_handling/handlers/player/DebugAIStateHandler.py
+++ game/world/opcode_handling/handlers/player/DebugAIStateHandler.py
@@ -1,7 +1,6 @@
 from struct import unpack, pack
 
 from game.world.managers.maps.MapManager import MapManager
-from game.world.managers.objects.ObjectManager import ObjectManager
 from network.packet.PacketWriter import PacketWriter
 from utils.constants.OpCodes import OpCode
 
@@ -15,7 +14,7 @@ class DebugAIStateHandler(object):
     def handle(world_session, socket, reader):
         if len(reader.data) >= 8:  # Avoid handling empty debug AI state packet.
             guid = unpack('<Q', reader.data[:8])[0]
-            world_object: ObjectManager = MapManager.get_surrounding_unit_by_guid(world_session.player_mgr, guid,
+            world_object = MapManager.get_surrounding_unit_by_guid(world_session.player_mgr, guid,
                                                                    include_players=True)
 
             # If no Unit or Player, try to get a Gameobject.
@@ -26,7 +25,7 @@ class DebugAIStateHandler(object):
             if not world_object:
                 return 0
 
-            messages: list[str] = world_object.get_debug_messages()
+            messages = world_object.get_debug_messages()
             data = pack(
                 '<QI',
                 guid,
