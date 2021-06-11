4f8882f04757d579a462d1c929a3e498b3bff020
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-06-11 10:35:36 -0500

    Fix swap item bug.

--- game/world/opcode_handling/handlers/inventory/SwapItemHandler.py
+++ game/world/opcode_handling/handlers/inventory/SwapItemHandler.py
@@ -7,5 +7,12 @@ class SwapItemHandler(object):
     def handle(world_session, socket, reader):
         if len(reader.data) >= 4:  # Avoid handling empty swap item packet.
             dest_bag, dest_slot, source_bag, source_slot = unpack('<4B', reader.data[:4])
+
+            if dest_bag == 255:
+                dest_bag = 23
+
+            if source_bag == 255:
+                source_bag = 23
+
             world_session.player_mgr.inventory.swap_item(source_bag, source_slot, dest_bag, dest_slot)
         return 0
