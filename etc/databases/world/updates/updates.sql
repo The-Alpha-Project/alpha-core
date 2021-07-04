9e4698d2efca73492bc389dd17b0666f1db4ad82
devwar87 <72774832+devwar87@users.noreply.github.com>
2021-07-03 19:06:18 -0500

    Relocate Ironforge and Darnassus FlightMasters based on dbc taxi info. (#135)
    * / Relocate Ironforge and Darnassus FlightMasters based on dbc taxi info.
    
    / Remove invalid spawns on old Darnassus FM location.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1388,5 +1388,21 @@ begin not atomic
         insert into applied_updates values ('280620211');
     end if;
 	
+	-- 03/07/2021 1
+	if (select count(*) from applied_updates where id='030720211') = 0 then
+		UPDATE `spawns_creatures` SET `position_x` = '-4650', `position_y` = '-1189.24', `position_z` = '503.79', `orientation` = '4.033' WHERE (`spawn_id` = '2008');
+		UPDATE `spawns_creatures` SET `position_x` = '9945.62', `position_y` = '2642.33', `position_z` = '1318', `orientation` = '1.60' WHERE (`spawn_id` = '49938');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '46825');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '49939');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '46821');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '46823');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '49459');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '49539');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '46824');
+		UPDATE `spawns_creatures` SET `ignored` = 1 WHERE (`spawn_id` = '46561');
+		
+		insert into applied_updates values ('030720211');
+    end if;
+	
 end $
 delimiter ;
