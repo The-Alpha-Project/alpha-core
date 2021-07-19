2f125f84de42f415165a770702fce16f22e0e294
devwar <72774832+devwar87@users.noreply.github.com>
2021-07-19 02:14:57 -0500

    Simple drowning damage.

--- game/world/managers/objects/timers/MirrorTimer.py
+++ game/world/managers/objects/timers/MirrorTimer.py
@@ -16,19 +16,14 @@ class MirrorTimer(object):
         self.chunk_elapsed = 0
 
     def start(self, elapsed):
-        self.remaining = self.duration
-        self.chunk_elapsed = int(elapsed)
-        self.active = True
-        self.send_full_update()
-
-    def send_full_update(self):
-        if self.active:
-            data = pack('<3IiBI', self.type, self.remaining, self.duration, self.scale, self.active, self.spell_id)
-            packet = PacketWriter.get_packet(OpCode.SMSG_START_MIRROR_TIMER, data)
-            self.owner.session.enqueue_packet(packet)
+        if not self.active and self.owner.is_alive:
+            self.remaining = self.duration
+            self.chunk_elapsed = int(elapsed)
+            self.active = True
+            self.send_full_update()
 
     def resume(self):
-        if not self.active:
+        if not self.active and self.owner.is_alive:
             self.active = True
             data = pack('<IB', self.type, not self.active)
             packet = PacketWriter.get_packet(OpCode.SMSG_PAUSE_MIRROR_TIMER, data)
@@ -48,18 +43,30 @@ class MirrorTimer(object):
             packet = PacketWriter.get_packet(OpCode.SMSG_STOP_MIRROR_TIMER, data)
             self.owner.session.enqueue_packet(packet)
 
+    def send_full_update(self):
+        if self.active:
+            data = pack('<3IiBI', self.type, self.remaining, self.duration, self.scale, self.active, self.spell_id)
+            packet = PacketWriter.get_packet(OpCode.SMSG_START_MIRROR_TIMER, data)
+            self.owner.session.enqueue_packet(packet)
+
     def set_scale(self, scale):
         if scale != self.scale:
             self.scale = scale
 
+    # TODO: Base timer class, children override update().
     def update(self, elapsed):
-        if self.active:
+        if self.active and self.owner.is_alive:
             self.chunk_elapsed += elapsed
             if self.chunk_elapsed >= self.interval:
                 if self.scale < 0:
                     self.remaining -= int(self.chunk_elapsed)
-                    if self.remaining < 0:
+                    if self.remaining <= 0:
                         self.remaining = 0
+                        # TODO: Find drowning damage formula.
+                        damage = int(self.owner.max_health * 0.10)
+                        new_health = self.owner.health - damage
+                        self.owner.set_health(new_health)
+                        self.owner.set_dirty()
                 else:
                     self.remaining += int(self.scale)
                     if self.remaining >= self.duration:
