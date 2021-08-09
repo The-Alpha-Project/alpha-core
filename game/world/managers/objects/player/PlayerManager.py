3fd47f6051b4a35c6ee7020cd58805ecf5b5fcba
devwar <72774832+devwar87@users.noreply.github.com>
2021-08-08 21:44:42 -0500

    / Partly revert e7baeacf903819bc9dcbe13d5b3becf5af2373b5.

--- game/world/managers/objects/player/PlayerManager.py
+++ game/world/managers/objects/player/PlayerManager.py
@@ -231,11 +231,7 @@ class PlayerManager(UnitManager):
         # Join default channels.
         ChannelManager.join_default_channels(self)
 
-        # Passive spells contain skill and proficiency learning.
-        # Perform passive spell casts after loading skills to avoid duplicate database entries.
-        self.spell_manager.cast_passive_spells()
-        self.skill_manager.init_proficiencies()
-        self.stat_manager.init_stats()
+        # Apply stats bonuses.
         self.stat_manager.apply_bonuses(replenish=first_login)
 
         # Init faction status.
