66f683eadf93231642a37489eb7e1b0fcca6ce7e
Fluglow <38987331+Fluglow@users.noreply.github.com>
2021-08-19 01:03:22 +0300

    Correct parameter name

--- game/world/opcode_handling/handlers/npc/TrainerBuySpellHandler.py
+++ game/world/opcode_handling/handlers/npc/TrainerBuySpellHandler.py
@@ -39,7 +39,7 @@ class TrainerBuySpellHandler(object):
                     spell_to_cast = DbcDatabaseManager.SpellHolder.spell_get_by_id(trainer_spell_id)
 
                     initialized_spell = world_session.player_mgr.spell_manager.try_initialize_spell(spell=spell_to_cast, caster_obj=world_session.player_mgr,
-                                                                                                    target=world_session.player_mgr, target_mask=SpellTargetMask.SELF,
+                                                                                                    spell_target=world_session.player_mgr, target_mask=SpellTargetMask.SELF,
                                                                                                     validate=False)
                     world_session.player_mgr.spell_manager.start_spell_cast(initialized_spell=initialized_spell)
 
