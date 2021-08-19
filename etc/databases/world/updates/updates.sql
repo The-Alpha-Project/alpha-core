2fd786d8f36379a89e3f3709524353ca3b5c2479
GrenderG <grenderg@gmail.com>
2021-08-20 01:09:37 +0200

    Drop unused table.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1,5 +1,10 @@
 delimiter $
 begin not atomic
+    -- 20/08/2021 1
+	if (select count(*) from applied_updates where id='200820211') = 0 then
+        DROP TABLE `reference_loot`;
 
+		insert into applied_updates values ('200820211');
+    end if;
 end $
 delimiter ;
