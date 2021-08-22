8bc2d05aece99efd29fab6ddb20a8e842adb8f14
Geo <72315006+geo-tp@users.noreply.github.com>
2021-08-22 03:07:44 +0200

    Stormwind GO & NPC fix (minor fix to match 0.5.3) (#188)

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -19,6 +19,67 @@ begin not atomic
                (2088, 2784, 0, 3597, -4592.550, -1095.895, 449.043, 2.362, 0, 86400, 86400);
 
         insert into applied_updates values ('210820211');
+
+    -- 20/08/2021 2
+    if (select count(*) from applied_updates where id='200820212') = 0 then
+        UPDATE `creature_template` SET `display_id1`=23 WHERE `entry`=4981;
+        UPDATE `creature_template` SET `display_id1`=2438 WHERE `entry`=1432;
+        UPDATE `creature_template` SET `display_id1`=20 WHERE `entry`=279;
+        UPDATE `creature_template` SET `display_id1`=190 WHERE `entry`=1212;
+        UPDATE `creature_template` SET `display_id1`=190 WHERE `entry`=1284;
+        UPDATE `creature_template` SET `display_id1`=1499 WHERE `entry`=928;
+        UPDATE `creature_template` SET `display_id1`=3445 WHERE `entry`=5566;
+        UPDATE `creature_template` SET `display_id1`=1573 WHERE `entry`=1444;
+        UPDATE `creature_template` SET `display_id1`=1541 WHERE `entry`=3516;
+        UPDATE `creature_template` SET `display_id1`=1480 WHERE `entry`=1307;
+        UPDATE `creature_template` SET `display_id1`=328 WHERE `entry`=1419;
+        UPDATE `creature_template` SET `display_id1`=4449 WHERE `entry`=173;
+        UPDATE `creature_template` SET `display_id1`=580 WHERE `entry`=1733;
+        UPDATE `creature_template` SET `display_id1`=213 WHERE `entry`=483;
+
+        -- # NPC SPAWN FIX
+        -- Elaine Trias was not present in the shop in 0.5.3, and Innkeeper Alliston didn't exist in 0.5.3, so because of
+        -- this screenshot: https://i.imgur.com/6zNt0SK.png we can *guess* that NPC might be Elaine Trias.
+        UPDATE `spawns_creatures` SET `position_y`=658.784,`position_x`=-8861.12,`position_z`=96.721, `orientation`=5.331 WHERE `spawn_id`=79665;
+        -- Jail Guards Z fix
+        UPDATE `spawns_creatures` SET `position_z`=99.193 WHERE `spawn_id`=79819;
+        UPDATE `spawns_creatures` SET `position_z`=99.126 WHERE `spawn_id`=19272;
+
+        -- # NPC SPAWN IGNORED
+        -- White kitten
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=7386;
+        -- Lil Timmy
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=8666;
+        -- Innkeepr Allison
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_id`=79841;
+        -- Lil Timmy
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=11867;
+        -- Woo Ping
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=11068;
+        -- Betty Quin
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=11068;
+        -- Kimberly Grant
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=11827;
+        -- Nara Meideros
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=11397;
+        -- Sprite Jumpsprocket
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=11026;
+        -- Randal Worth
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=11096;
+        -- Royal Factor Barthrillor
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=10782;
+        -- Jalane Ayrole
+        UPDATE `spawns_creatures` SET `ignored`=1 WHERE `spawn_entry1`=9584;
+
+        -- # GAMEOBJECTS DISPLAY FIX
+        -- Books from SW Keep library
+        UPDATE `gameobject_template` SET `displayId`=558 WHERE `entry`=175735;
+
+        -- # GAMEOBJECTS SPAWNS IGNORED
+        -- Those gameobjects spawns are not part of 0.5.3, so we hide or delete
+        UPDATE `spawns_gameobjects` SET `ignored`=1 WHERE `spawn_entry` IN (140911, 103795, 2123, 2190, 160444, 164908, 179731, 179728, 24469, 24470, 160442, 24653, 24654, 24686, 160443, 179730, 179733, 179727,179729, 179726, 179734, 179735, 176576, 179737, 179738);
+
+        insert into applied_updates values ('200820212');
     end if;
 end $
 delimiter ;
