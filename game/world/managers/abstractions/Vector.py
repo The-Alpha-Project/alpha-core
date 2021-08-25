d55d8c1f59e5f2f980b8eebbaa22969786144e02
GrenderG <grenderg@gmail.com>
2021-08-26 00:06:56 +0200

    Typo.

--- game/world/managers/abstractions/Vector.py
+++ game/world/managers/abstractions/Vector.py
@@ -22,7 +22,7 @@ class Vector(object):
         return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def __str__(self):
-        return f'{self.x}, {self.y}, {self.z}. {self.o}'
+        return f'{self.x}, {self.y}, {self.z}, {self.o}'
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y and self.z == other.z
