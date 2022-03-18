f6bbc0a1b06fbccfabcda97c12a37e9ba0f903ce
devwar87 <72774832+devwar87@users.noreply.github.com>
2022-03-18 05:55:09 -0600

    Update CreatureManager.py

--- game/world/managers/objects/units/creature/CreatureManager.py
+++ game/world/managers/objects/units/creature/CreatureManager.py
@@ -378,7 +378,6 @@ class CreatureManager(UnitManager):
         self.set_uint32(UnitFields.UNIT_FIELD_HEALTH, self.health)
         self.set_uint32(UnitFields.UNIT_FIELD_MAXHEALTH, self.max_health)
         self.set_uint32(UnitFields.UNIT_FIELD_POWER1, self.power_1)
-        self.set_uint32(UnitFields.UNIT_FIELD_MAXHEALTH, self.max_health)
         self.set_uint32(UnitFields.UNIT_FIELD_MAXPOWER1, self.max_power_1)
         self.set_uint32(UnitFields.UNIT_FIELD_LEVEL, self.level)
         self.set_uint32(UnitFields.UNIT_FIELD_FACTIONTEMPLATE, self.faction)
