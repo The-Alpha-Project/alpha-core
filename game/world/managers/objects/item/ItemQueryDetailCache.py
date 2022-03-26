001efc4326aa5356363792a33aab2c65d9bc4b96
devwar87 <72774832+devwar87@users.noreply.github.com>
2022-03-25 22:12:02 -0600

    Update ItemQueryDetailCache.py

--- game/world/managers/objects/item/ItemQueryDetailCache.py
+++ game/world/managers/objects/item/ItemQueryDetailCache.py
@@ -1,7 +1,10 @@
 from game.world.managers.objects.item.ItemManager import ItemManager
 
+
+# Use this cache when sending item detail queries for items that doesn't have an owner (Item instance) yet.
 class ItemQueryDetailCache(object):
     CACHED_ITEM_DETAIL_QUERY_PACKET = {}
+
     @staticmethod
     def get_item_detail_query(item_template):
         if item_template.entry in ItemQueryDetailCache.CACHED_ITEM_DETAIL_QUERY_PACKET:
