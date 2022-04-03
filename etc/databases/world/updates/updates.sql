6c4a03b5775b13fc9e90279cd394aec49d9aed36
devwar87 <72774832+devwar87@users.noreply.github.com>
2022-04-03 14:25:02 -0500

    Update updates.sql

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -12,11 +12,11 @@ begin not atomic
 	-- 03/04/2022 2
 	if (select count(*) from applied_updates where id='030420222') = 0 then
         -- Teleport to Stormwind
-        UPDATE `alpha_world`.`spell_target_position` SET `target_position_z` = '129.72' WHERE (`id` = '3561') and (`target_map` = '0');
+        UPDATE `spell_target_position` SET `target_position_z` = '129.72' WHERE (`id` = '3561') and (`target_map` = '0');
         -- Teleport to Ironforge
-        UPDATE `alpha_world`.`spell_target_position` SET `target_position_z` = '519.30' WHERE (`id` = '3562') and (`target_map` = '0');
-        -- Teleport to Orgrimmar (X,Y placement is still wrong in this one, but player wont fall throught the world anymore)
-        UPDATE `alpha_world`.`spell_target_position` SET `target_position_z` = '120.99' WHERE (`id` = '3567') and (`target_map` = '1');
+        UPDATE `spell_target_position` SET `target_position_z` = '519.30' WHERE (`id` = '3562') and (`target_map` = '0');
+        -- Teleport to Orgrimmar
+        UPDATE `spell_target_position` SET `target_position_x` = '1552.5', `target_position_y` = '-4420.66', `target_position_z` = '8.94802' WHERE (`id` = '3567') and (`target_map` = '1');
 		
         insert into applied_updates values ('030420222');
     end if;
