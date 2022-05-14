bad2c3c3fb57f9baa9458a9cfbd35d847360214c
Fluglow <38987331+Fluglow@users.noreply.github.com>
2022-05-14 16:53:48 +0300

    Revert changes to object field setters

--- game/world/managers/objects/ObjectManager.py
+++ game/world/managers/objects/ObjectManager.py
@@ -259,31 +259,36 @@ class ObjectManager:
         return UnitFields.UNIT_FIELD_AURA <= index <= UnitFields.UNIT_FIELD_AURA + 55
 
     def set_int32(self, index, value):
-        self.update_packet_factory.update(index, value, 'i')
+        if self.update_packet_factory.should_update(index, value, 'i'):
+            self.update_packet_factory.update(index, value, 'i')
 
     def get_int32(self, index):
         return self._get_value_by_type_at('i', index)
 
     def set_uint32(self, index, value):
-        self.update_packet_factory.update(index, value, 'I')
+        if self.update_packet_factory.should_update(index, value, 'I'):
+            self.update_packet_factory.update(index, value, 'I')
 
     def get_uint32(self, index):
         return self._get_value_by_type_at('I', index)
 
     def set_int64(self, index, value):
-        self.update_packet_factory.update(index, value, 'q')
+        if self.update_packet_factory.should_update(index, value, 'q'):
+            self.update_packet_factory.update(index, value, 'q')
 
     def get_int64(self, index):
         return self._get_value_by_type_at('q', index)
 
     def set_uint64(self, index, value):
-        self.update_packet_factory.update(index, value, 'Q')
+        if self.update_packet_factory.should_update(index, value, 'Q'):
+            self.update_packet_factory.update(index, value, 'Q')
 
     def get_uint64(self, index):
         return self._get_value_by_type_at('Q', index)
 
     def set_float(self, index, value):
-        self.update_packet_factory.update(index, value, 'f')
+        if self.update_packet_factory.should_update(index, value, 'f'):
+            self.update_packet_factory.update(index, value, 'f')
 
     def get_float(self, index):
         return self._get_value_by_type_at('f', index)
