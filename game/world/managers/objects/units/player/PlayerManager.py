9272cd9b9872c85833a8326f8d34d566db291018
devwar <108442943+devw4r@users.noreply.github.com>
2022-08-04 23:42:00 -0500

    Update PlayerManager.py

--- game/world/managers/objects/units/player/PlayerManager.py
+++ game/world/managers/objects/units/player/PlayerManager.py
@@ -810,7 +810,9 @@ class PlayerManager(UnitManager):
             # Slot should match real current_loot indexes.
             for loot in loot_manager.current_loot:
                 if loot:
-                    # If this is a quest item and player does not need it, don't show it to this player.
+                    # Skip conditions:
+                    # Is item quest and player does not have the involved quest.
+                    # Is quest multi-drop item and is no longer visible to this player.
                     if loot.is_quest_item() and \
                             not self.player_or_group_require_quest_item(loot.get_item_entry(), only_self=True) or \
                             not loot.is_visible_to_player(self):
