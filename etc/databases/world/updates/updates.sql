5dcc5258944f46b46bdbb32d26a4aaf8aa963ac8
geo <tp53.geo@gmail.com>
2022-08-09 06:34:41 +0200

    Silverpine improvements

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -8782,6 +8782,59 @@ begin not atomic
 
         insert into applied_updates values ('080820223');
     end if;
+
+    -- 09/08/2022 1
+    if (select count(*) from applied_updates where id='090820221') = 0 then
+
+        -- SILVERPINE
+
+        -- Worg
+        UPDATE `creature_template`
+        SET `display_id1`=246
+        WHERE `entry`=1765;
+
+        -- Moonrage Whitescalp
+        UPDATE `creature_template`
+        SET `display_id1`=729
+        WHERE `entry`=1769;
+
+        -- Rot Hide Plague Weaver
+        UPDATE `creature_template`
+        SET `display_id1`=858
+        WHERE `entry`=1940;
+
+        -- Rot Hide Brute
+        UPDATE `creature_template`
+        SET `display_id1`=858
+        WHERE `entry`=1939;
+
+        -- Rot Hide Mystic
+        UPDATE `creature_template`
+        SET `display_id1`=847
+        WHERE `entry`=1773;
+
+        -- Raging Rot Hide
+        UPDATE `creature_template`
+        SET `display_id1`=858
+        WHERE `entry`=1943;
+
+        -- Thule Ravenclaw
+        UPDATE `creature_template`
+        SET `display_id1`=859
+        WHERE `entry`=1947;
+
+        -- Lake Skulker
+        UPDATE `creature_template`
+        SET `display_id1`=862
+        WHERE `entry`=1953;
+
+        -- Elder Lake Skulker
+        UPDATE `creature_template`
+        SET `display_id1`=863
+        WHERE `entry`=1954;
+
+        insert into applied_updates values ('090820221');
+    end if;
 end $
 delimiter ;
 
