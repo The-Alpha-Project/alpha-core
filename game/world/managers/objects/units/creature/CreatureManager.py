368e99c8091858e5228741dd3440918b5fa6a3ac
devw4r <108442943+devw4r@users.noreply.github.com>
2022-08-19 06:51:13 -0500

    / Fixes (#516)
    Fix 'In Combat', again.
    Fix evade waypoints being flushed before usage.

--- game/world/managers/objects/units/creature/CreatureManager.py
+++ game/world/managers/objects/units/creature/CreatureManager.py
@@ -618,6 +618,8 @@ class CreatureManager(UnitManager):
 
         # Get the path we are using to get back to spawn location.
         waypoints_to_spawn, z_locked = self._get_return_to_spawn_points()
+        self.evading_waypoints.clear()
+
         # Despawn this creature if the last evade point is too far away from its current position.
         if self.location.distance(waypoints_to_spawn[-1]) > Distances.CREATURE_EVADE_DISTANCE * 2:
             self.despawn()
