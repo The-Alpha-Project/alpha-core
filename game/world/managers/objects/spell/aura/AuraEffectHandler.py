e8f391031e79ce380a9b73a68943fda0bc3cb9e9
Fluglow <38987331+Fluglow@users.noreply.github.com>
2022-08-22 03:54:05 +0300

    Call remove for proc aura effects

--- game/world/managers/objects/spell/aura/AuraEffectHandler.py
+++ game/world/managers/objects/spell/aura/AuraEffectHandler.py
@@ -24,7 +24,7 @@ class AuraEffectHandler:
                          f'{aura.spell_effect.aura_type}) from spell {aura.source_spell.spell_entry.ID}.')
             return
 
-        if not is_proc and aura_type in PROC_AURA_EFFECTS:
+        if not remove and not is_proc and aura_type in PROC_AURA_EFFECTS:
             return  # Only call proc effects when a proc happens.
 
         AURA_EFFECTS[aura.spell_effect.aura_type](aura, effect_target, remove)
