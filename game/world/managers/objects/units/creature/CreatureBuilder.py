7d7d771026b301d56712b060c0c170d5360efe9a
devwar <108442943+devw4r@users.noreply.github.com>
2022-09-11 01:07:21 -0500

    / Fix pet summon/taming.

--- game/world/managers/objects/units/creature/CreatureBuilder.py
+++ game/world/managers/objects/units/creature/CreatureBuilder.py
@@ -22,20 +22,20 @@ class CreatureBuilder:
         creature_instance.entry = creature_template.entry
         creature_instance.guid = creature_instance.generate_object_guid(CreatureBuilder.GUID_MANAGER.get_new_guid())
         creature_instance.creature_template = creature_template
+        creature_instance.summoner = summoner
+        creature_instance.subtype = subtype
+        creature_instance.mana_percent = mana_percent
+        creature_instance.health_percent = health_percent
 
         # Initialize from gameobject template.
         creature_instance.initialize_from_creature_template(creature_template)
 
-        # Continue initialization.
+        # Continue initialization, order above matters.
         creature_instance.faction = faction if faction else creature_template.faction
         creature_instance.location = location
         creature_instance.spawn_position = creature_instance.location.copy()
         creature_instance.map_ = map_id if not summoner else summoner.map_
         creature_instance.zone = summoner.zone if summoner else 0
-        creature_instance.health_percent = health_percent
-        creature_instance.mana_percent = mana_percent
-        creature_instance.summoner = summoner
-        creature_instance.subtype = subtype
         creature_instance.spell_id = spell_id
         creature_instance.time_to_live_timer = ttl
         creature_instance.addon = addon
