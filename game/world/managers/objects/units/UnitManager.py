c9dd68c66b3db5185ab43e9c72a6c5ef1edc469a
GrenderG <grenderg@gmail.com>
2022-11-12 02:58:01 +0100

    Typo.

--- game/world/managers/objects/units/UnitManager.py
+++ game/world/managers/objects/units/UnitManager.py
@@ -85,7 +85,7 @@ class UnitManager(ObjectManager):
                  resistance_4=0,
                  resistance_5=0,
                  stand_state=0,
-                 sheathe_state=WeaponMode.SHEATHEDMODE,
+                 sheath_state=WeaponMode.SHEATHEDMODE,
                  shapeshift_form=0,
                  bytes_1=0,  # stand state, shapeshift form, sheathstate
                  dynamic_flags=0,
@@ -156,7 +156,7 @@ class UnitManager(ObjectManager):
         self.resistance_4 = resistance_4
         self.resistance_5 = resistance_5
         self.stand_state = stand_state
-        self.sheath_state = sheathe_state
+        self.sheath_state = sheath_state
         self.shapeshift_form = shapeshift_form
         self.bytes_1 = bytes_1  # stand state, shapeshift form, sheathstate
         self.dynamic_flags = dynamic_flags
