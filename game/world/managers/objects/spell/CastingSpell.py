4f7cf42cf9db52aedd67262be760701c7b737b8f
devw4r <108442943+devw4r@users.noreply.github.com>
2022-11-23 23:59:35 -0600

    Update CastingSpell.py

--- game/world/managers/objects/spell/CastingSpell.py
+++ game/world/managers/objects/spell/CastingSpell.py
@@ -94,6 +94,11 @@ class CastingSpell:
             self.initial_target = MapManager.find_liquid_location_in_range(self.spell_caster,
                                                                            self.range_entry.RangeMin,
                                                                            self.range_entry.RangeMax)
+
+        if self.targets_owner_pet():
+            pet = self.spell_caster.get_pet()
+            self.initial_target = pet if pet else self.initial_target
+
         self.cast_flags = SpellCastFlags.CAST_FLAG_NONE
 
         # Ammo needs to be resolved on initialization since it's needed for validation and spell cast packets.
@@ -307,6 +312,9 @@ class CastingSpell:
 
     def is_fishing_spell(self):
         return self.spell_entry.ImplicitTargetA_1 == SpellImplicitTargets.TARGET_SELF_FISHING
+
+    def targets_owner_pet(self):
+        return self.spell_entry.ImplicitTargetA_1 == SpellImplicitTargets.TARGET_PET
 
     def is_pick_pocket_spell(self):
         return self.spell_entry.AttributesEx & SpellAttributesEx.SPELL_ATTR_EX_FAILURE_BREAKS_STEALTH
