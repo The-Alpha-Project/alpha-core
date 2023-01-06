79644e58f4449f05ef8849bd6197795c31a14770
radongc <ccostanzo115@gmail.com>
2023-01-06 01:19:57 -0500

    Fix condition causing talent availability bug

--- game/world/managers/objects/units/creature/utils/TrainerUtils.py
+++ game/world/managers/objects/units/creature/utils/TrainerUtils.py
@@ -134,11 +134,9 @@ class TrainerUtils:
             return TrainerServices.TRAINER_SERVICE_USED
 
         # target.skill_manager.get_total_skill_value(skill_id)
-        if (not fulfills_skill or (preceded_spell and preceded_spell not in target_spells) or
-            trainer_spell_template.reqskill != 0) \
-                and ((not player_mgr.skill_manager.has_skill(trainer_spell_template.reqskill)
-                      or player_mgr.skill_manager.get_total_skill_value(trainer_spell_template.reqskill)
-                      < trainer_spell_template.reqskillvalue)):
+        if not fulfills_skill or (preceded_spell and preceded_spell not in target_spells) or \
+                trainer_spell_template.reqskill != 0 and (not player_mgr.skill_manager.has_skill(trainer_spell_template.reqskill) or \
+                player_mgr.skill_manager.get_total_skill_value(trainer_spell_template.reqskill) < trainer_spell_template.reqskillvalue):
             return TrainerServices.TRAINER_SERVICE_UNAVAILABLE
 
         if player_mgr.level >= req_level:
