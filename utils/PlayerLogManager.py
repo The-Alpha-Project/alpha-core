df08d26b518b649f767c3e96e457f1fec079fe9b
radongc <ccostanzo115@gmail.com>
2023-01-09 14:42:17 -0500

    Refactors

--- utils/PlayerLogManager.py
+++ utils/PlayerLogManager.py
@@ -11,44 +11,45 @@ class PlayerLogManager:
     @staticmethod
     def log_chat(player_mgr, msg, chat_type):
         if config.Server.Logging.log_player_chat:
-            Path(PlayerLogManager.LOG_PATH).mkdir(parents=True, exist_ok=True)
-            with open ('etc/logs/chat.log', 'a+') as log:
-                date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-                log_type = None
+            date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+            log_type = None
 
-                if chat_type == ChatMsgs.CHAT_MSG_SAY:
-                    log_type = 'Say'
-                elif chat_type == ChatMsgs.CHAT_MSG_YELL:
-                    log_type = 'Yell'
-                elif chat_type == ChatMsgs.CHAT_MSG_EMOTE:
-                    log_type = 'Emote'
-                elif chat_type == ChatMsgs.CHAT_MSG_PARTY:
-                    log_type = 'Party'
-                elif chat_type == ChatMsgs.CHAT_MSG_GUILD:
-                    log_type = 'Guild'
-                elif chat_type == ChatMsgs.CHAT_MSG_OFFICER:
-                    log_type = 'Officer'
+            if chat_type == ChatMsgs.CHAT_MSG_SAY:
+                log_type = 'Say'
+            elif chat_type == ChatMsgs.CHAT_MSG_YELL:
+                log_type = 'Yell'
+            elif chat_type == ChatMsgs.CHAT_MSG_EMOTE:
+                log_type = 'Emote'
+            elif chat_type == ChatMsgs.CHAT_MSG_PARTY:
+                log_type = 'Party'
+            elif chat_type == ChatMsgs.CHAT_MSG_GUILD:
+                log_type = 'Guild'
+            elif chat_type == ChatMsgs.CHAT_MSG_OFFICER:
+                log_type = 'Officer'
 
-                log.write(f'{date} [CHAT] (GUID {player_mgr.guid}, NAME {player_mgr.get_name()}, MAP {player_mgr.map_}, POS {PlayerLogManager._get_location_string(player_mgr)}): [{log_type}] {player_mgr.get_name()}:{player_mgr.guid} : {msg}\n')
-                return
+            PlayerLogManager._write_to_log(f'{date} [CHAT] (GUID {player_mgr.guid}, NAME {player_mgr.get_name()}, MAP {player_mgr.map_}, POS {PlayerLogManager._get_location_string(player_mgr)}): [{log_type}] {player_mgr.get_name()}:{player_mgr.guid} : {msg}')
+            return
 
     @staticmethod
     def log_channel(player_mgr, msg, channel_name):
         if config.Server.Logging.log_player_chat:
-            Path(PlayerLogManager.LOG_PATH).mkdir(parents=True, exist_ok=True)
-            with open (f'{PlayerLogManager.LOG_PATH}/{PlayerLogManager.CHAT_LOG_FILE}', 'a+') as log:
-                date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-                log.write(f'{date} [CHAT] (GUID {player_mgr.guid}, NAME {player_mgr.get_name()}, MAP {player_mgr.map_}, POS {PlayerLogManager._get_location_string(player_mgr)}): [Channel: {channel_name}] {player_mgr.get_name()}:{player_mgr.guid} : {msg}\n')
-                return
+            date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+            PlayerLogManager._write_to_log(f'{date} [CHAT] (GUID {player_mgr.guid}, NAME {player_mgr.get_name()}, MAP {player_mgr.map_}, POS {PlayerLogManager._get_location_string(player_mgr)}): [Channel: {channel_name}] {player_mgr.get_name()}:{player_mgr.guid} : {msg}')
+            return
 
     @staticmethod
     def log_whisper(player_mgr, msg, target_player_mgr):
         if config.Server.Logging.log_player_chat:
-            Path(PlayerLogManager.LOG_PATH).mkdir(parents=True, exist_ok=True)
-            with open (f'{PlayerLogManager.LOG_PATH}/{PlayerLogManager.CHAT_LOG_FILE}', 'a+') as log:
-                date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-                log.write(f'{date} [CHAT] (GUID {player_mgr.guid}, NAME {player_mgr.get_name()}, MAP {player_mgr.map_}, POS {PlayerLogManager._get_location_string(player_mgr)}): [Whisper] {player_mgr.get_name()}:{player_mgr.guid} -> {target_player_mgr.get_name()}:{target_player_mgr.guid} : {msg}\n')
-                return
+            date = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+            PlayerLogManager._write_to_log(f'{date} [CHAT] (GUID {player_mgr.guid}, NAME {player_mgr.get_name()}, MAP {player_mgr.map_}, POS {PlayerLogManager._get_location_string(player_mgr)}): [Whisper] {player_mgr.get_name()}:{player_mgr.guid} -> {target_player_mgr.get_name()}:{target_player_mgr.guid} : {msg}\n')
+            return
+
+    @staticmethod
+    def _write_to_log(msg):
+        Path(PlayerLogManager.LOG_PATH).mkdir(parents=True, exist_ok=True)
+        with open (f'{PlayerLogManager.LOG_PATH}/{PlayerLogManager.CHAT_LOG_FILE}', 'a+') as log:
+            log.write(f'{msg}\n')
+            log.close()
 
     @staticmethod
     def _get_location_string(player_mgr):
