238e44f7b0515692208a4c8d6e90e5bcba21d1a3
radongc <ccostanzo115@gmail.com>
2023-01-09 23:55:26 -0500

    Add summon and port console commands

---
+++ game/world/opcode_handling/handlers/player/cheats/TeleportToPlayerHandler.py
@@ -0,0 +1,26 @@
+from game.world.managers.CommandManager import CommandManager
+from game.world.managers.objects.units.player.ChatManager import ChatManager
+from game.world.opcode_handling.HandlerValidator import HandlerValidator
+from network.packet.PacketReader import *
+from network.packet.PacketWriter import *
+from utils.Logger import Logger
+
+
+class TeleportToPlayerHandler(object):
+
+    @staticmethod
+    def handle(world_session, socket, reader: PacketReader) -> int:
+        # Validate world session.
+        player_mgr, res = HandlerValidator.validate_session(world_session, reader.opcode, disconnect=False)
+        if not player_mgr:
+            return res
+
+        if not player_mgr.session.account_mgr.is_gm():
+            Logger.anticheat(f'Player {player_mgr.get_name()} ({player_mgr.guid}) tried to port to a player.')
+            return 0
+
+        if len(reader.data) >= 1:  # Avoid handling empty teleport to player packet.
+            player_name: str = PacketReader.read_string(reader.data, 0)
+            CommandManager.goplayer(world_session, player_name)
+
+        return 0
