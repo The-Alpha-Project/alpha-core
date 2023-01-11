00bae2855e420c3f84da831db1bb8c86d66567e9
Fluglow <38987331+Fluglow@users.noreply.github.com>
2023-01-11 19:20:01 +0200

    Use direct access with instance id

--- game/world/managers/maps/MapManager.py
+++ game/world/managers/maps/MapManager.py
@@ -154,7 +154,7 @@ class MapManager:
     @staticmethod
     def get_map_by_object(world_object):
         try:
-            return MAPS[world_object.map_id].get(world_object.instance_id)
+            return MAPS[world_object.map_id][world_object.instance_id]
         except (KeyError, AttributeError, TypeError):
             Logger.error(f'Unable to retrieve Map for Id {world_object.map_id}, Instance {world_object.instance_id}')
             return None
