064ba6e9aeecce2692fc4858d3d1f163d9d91b74
devw4r <108442943+devw4r@users.noreply.github.com>
2023-02-05 22:39:04 -0600

    Fix teleport race condition.

--- game/world/managers/maps/helpers/PendingTeleportDataHolder.py
+++ game/world/managers/maps/helpers/PendingTeleportDataHolder.py
@@ -9,7 +9,3 @@ class PendingTeleportDataHolder:
     origin_map: int
     destination_location: Vector
     destination_map: int
-    failed: bool
-
-    def set_failed(self):
-        self.failed = True
