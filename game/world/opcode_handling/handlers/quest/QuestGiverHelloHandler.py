cbaf501c76dd3b8ab2565d1baf157e4cd237fbe1
Richard Kämmerer <richard@richardkaemmerer.de>
2023-03-20 13:25:04 +0100

    Pause NPC movement when a player talks to them (#990)
    * Pause NPC movement when a player talks to them
    
    * Pause movement for 3 minutes
    
    ---------
    
    Co-authored-by: Richard Kämmerer <richard.kaemmerer@gmail.com>

--- game/world/opcode_handling/handlers/quest/QuestGiverHelloHandler.py
+++ game/world/opcode_handling/handlers/quest/QuestGiverHelloHandler.py
@@ -2,7 +2,7 @@ from struct import unpack
 from game.world.managers.maps.MapManager import MapManager
 from game.world.opcode_handling.HandlerValidator import HandlerValidator
 from utils.GuidUtils import GuidUtils
-from utils.constants.MiscCodes import HighGuid
+from utils.constants.MiscCodes import HighGuid, ObjectTypeIds
 from utils.Logger import Logger
 
 
@@ -40,10 +40,12 @@ class QuestGiverHelloHandler(object):
                 Logger.warning(f'{reader.opcode_str()}, quest giver with guid: {guid} is hostile.')
                 return 0
 
-            # TODO: Stop the npc if it's moving
             # TODO: Remove feign death from player
             # TODO: If the gossip menu is already open, do nothing
             if is_item or quest_giver.is_within_interactable_distance(player_mgr):
+                # Pause the NPCs movement when a player attempts to talk to them
+                if quest_giver.get_type_id() == ObjectTypeIds.ID_UNIT:
+                    quest_giver.movement_manager.try_pause_ooc_movement(180)
                 player_mgr.quest_manager.handle_quest_giver_hello(quest_giver, guid)
 
         return 0
