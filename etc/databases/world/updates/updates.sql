57c2d3dcadda6fdc83d9739330ca8ada5ccea502
Richard Kämmerer <richard@richardkaemmerer.de>
2023-03-25 11:35:21 +0100

    Fix SQL comments (#1042)
    * Fix SQL comments
    
    * Fix more SQL comments
    
    ---------
    
    Co-authored-by: Richard Kämmerer <richard.kaemmerer@gmail.com>

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1042,12 +1042,12 @@ begin not atomic
 
     -- 22/03/2023 1
 	if(select count(*) from applied_updates where id = '220320231') = 0 then
-		--Despawn Clara's Fresh Apples since the model and quest don't exist
+		-- Despawn Clara's Fresh Apples since the model and quest don't exist
 		update spawns_gameobjects set ignored = 1 where spawn_entry = 142076;
 
-		--Set human NPCs to the correct faction
-		update creature_template set faction = 11 where entry = 1649; --Jordan Croft
-		update creature_template set faction = 11 where entry = 2939; --Jackson Bayne
+		-- Set human NPCs to the correct faction
+		update creature_template set faction = 11 where entry = 1649; -- Jordan Croft
+		update creature_template set faction = 11 where entry = 2939; -- Jackson Bayne
 
 		insert into applied_updates values ('220320231');
 	end if;
