702b1769a523bd156810909d6420984c236309b3
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-03-27 15:30:01 +0200

    Closes #1062

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1474,5 +1474,38 @@ begin not atomic
 		insert into applied_updates values ('260320231');
 	end if;
 
+    -- 27/03/2023 1
+    if(select count(*) from applied_updates where id = '270320231') = 0 then
+        -- Set subname for Brock Stoneseeker
+        update creature_template set subname = "Cartography Trainer" where entry = 1681;
+        -- Set subname for Karm Ironquill
+        update creature_template set subname = "Cartography Supplies" where entry = 1764;
+        -- Set subname for Berte & Evalyn
+        update creature_template set subname = "Needs Model" where entry in(1880, 1881);
+        -- Set faction for Evalyn
+        update creature_template set faction = 35 where entry = 1881;
+        -- Spawn Berte & Evalyn
+        insert into spawns_creatures (spawn_entry1, position_x, position_y, position_z, orientation) VALUES
+            (1880, -5333.290, -2955.981, 326.585, 3.444),
+            (1881, -5337.208, -2957.187, 325.299, 3.469);
+        -- Set faction and level 20 stats for Noma Bluntnose (identical to her husband Magistrate Bluntnose)
+        update creature_template set faction = 57, level_min = 20, level_max = 20, health_min = 484, health_max = 484, armor = 852 where entry = 1879;
+        -- Spawn Noma Bluntnose
+        insert into spawns_creatures (spawn_entry1, position_x, position_y, position_z, orientation) VALUES
+            (1879, -5296.893, -2953.931, 336.758, 2.971);
+
+        -- make squirrels small again! and non-humanoid for a change!
+        -- ... and Private Merle should not be a squirrel either.
+        update creature_template set display_id1 = 134 where entry = 1412;
+        update creature_template set display_id1 = 173 where entry = 1421;
+
+        -- move Whaldak Darkbenk <Spider Trainer> to his true location
+        update spawns_creatures set position_x = -4828.347, position_y = -2716.226, position_z = 328.332, orientation = 1.585 where spawn_id = 400071;
+        -- set proper faction and have him spawn his spider pet
+        update creature_template set faction = 57, spell_id1 = 7912 where entry = 2872;
+
+        insert into applied_updates values ('270320231');
+    end if;
+
 end $
 delimiter ;
