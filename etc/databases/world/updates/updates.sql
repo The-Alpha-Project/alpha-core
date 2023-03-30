75871a7cd8e5f8e2ecf73b2ae10c56270a819362
Geo <72315006+geo-tp@users.noreply.github.com>
2023-03-30 13:13:59 +0200

    Various NPCs Fixes (#1082)
    * Theramore and Hinterlands improvements
    
    * add defias renegade
    
    * type
    
    ---------
    
    Co-authored-by: GrenderG <grenderg@gmail.com>

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1,8 +1,6 @@
 delimiter $
 begin not atomic
 
-
-
     -- 27/03/2023 1
     if(select count(*) from applied_updates where id = '270320231') = 0 then
         -- Set subname for Brock Stoneseeker
@@ -34,6 +32,141 @@ begin not atomic
         update creature_template set faction = 57, spell_id1 = 7912 where entry = 2872;
 
         insert into applied_updates values ('270320231');
+    end if;
+
+    -- 29/03/2023 1
+    if (select count(*) from `applied_updates` where id='290320231') = 0 then
+
+        -- Alchemist Narett
+        UPDATE `creature_template` SET `display_id1`=18 WHERE `entry`=4900;
+
+        -- Brant Jasperbloom
+        UPDATE `creature_template` SET `display_id1`=226 WHERE `entry`=4898;
+
+        -- Uma Barthum
+        UPDATE `creature_template` SET `display_id1`=15 WHERE `entry`=4899;
+
+        -- Hans Weston
+        UPDATE `creature_template` SET `display_id1`=17 WHERE `entry`=4886;
+
+        -- Marie Holdston
+        UPDATE `creature_template` SET `display_id1`=327 WHERE `entry`=4888;
+
+        -- Gregor MacVince
+        UPDATE `creature_template` SET `display_id1`=107 WHERE `entry`=4885;
+
+        -- Helenia Olden
+        UPDATE `creature_template` SET `display_id1`=162 WHERE `entry`=4897;
+
+        -- Jensen Farran
+        UPDATE `creature_template` SET `display_id1`=106 WHERE `entry`=4892;
+
+        -- Guard Lasiter
+        UPDATE `creature_template` SET `display_id1`=3138 WHERE `entry`=4973;
+
+        -- Piter Verance
+        UPDATE `creature_template` SET `display_id1`=17 WHERE `entry`=4890;
+
+        -- Torq Ironblast
+        UPDATE `creature_template` SET `display_id1`=2584 WHERE `entry`=4889;
+
+        -- Dwane Wertle
+        UPDATE `creature_template` SET `display_id1`=285 WHERE `entry`=4891;
+
+        -- Morgan Stern
+        UPDATE `creature_template` SET `display_id1`=280 WHERE `entry`=4794;
+
+        -- Craig Nollward
+        UPDATE `creature_template` SET `display_id1`=126 WHERE `entry`=4894;
+
+        -- Bartender Lillian
+        UPDATE `creature_template` SET `display_id1`=1049 WHERE `entry`=4893;
+
+        -- Ingo Woolybush
+        UPDATE `creature_template` SET `display_id1`=2584 WHERE `entry`=5388;
+
+        -- Theramore Lieutenant
+        UPDATE `creature_template` SET `display_id1` = 2981, `display_id2`= 2982, `display_id3` = 2983, `display_id4` = 2984 where `entry` = 4947;
+
+        -- Theramore Skirmisher
+        UPDATE `creature_template` SET `display_id1` = 2977, `display_id2` = 2978, `display_id3` = 2979, `display_id4` = 2980 where `entry` = 5044;
+
+        insert into`applied_updates`values ('290320231');
+    end if;
+
+    -- 29/03/2023 3
+    if (select count(*) from `applied_updates` where id='290320233') = 0 then
+
+        -- Vilebranch Axe Trower
+        UPDATE `creature_template` SET `display_id1` = 590, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2639;
+
+        -- Vilebranch Witch Doctor
+        UPDATE `creature_template` SET `display_id1` = 1117, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2640;
+
+        -- Vilebranch Headhunter
+        UPDATE `creature_template` SET `display_id1` = 1113, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2641;
+
+        -- Vilebranch Shadowcaster
+        UPDATE `creature_template` SET `display_id1` = 1115, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2642;
+
+        -- Vilebranch Berserker
+        UPDATE `creature_template` SET `display_id1` = 1118, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2643;
+
+        -- Vilebranch Hideskinner
+        UPDATE `creature_template` SET `display_id1` = 1154, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2644;
+
+        -- Vilebranch Shadow Hunter
+        UPDATE `creature_template` SET `display_id1` = 590, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2645;
+
+        -- Vilebranch Blood Drinker
+        UPDATE `creature_template` SET `display_id1` = 1118, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2646;
+
+        -- Vilebranch Soul Eater
+        UPDATE `creature_template` SET `display_id1` = 590, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2647;
+
+        -- Vilebranch Aman'zasi Guard
+        UPDATE `creature_template` SET `display_id1` = 1151, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2648;
+
+        -- Whiterbark Scalper
+        UPDATE `creature_template` SET `display_id1` = 337, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2649;
+
+        -- Whiterbark Zealot
+        UPDATE `creature_template` SET `display_id1` = 1113, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2650;
+
+        -- Wetherbark Hideskinner
+        UPDATE `creature_template` SET `display_id1` = 337, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2651;
+
+        -- Wetherbark Venomblood
+        UPDATE `creature_template` SET `display_id1` = 1116, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2652;
+
+        -- Wetherbark Sadist
+        UPDATE `creature_template` SET `display_id1` = 1114, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2653;
+
+        -- Wetherbark Caller
+        UPDATE `creature_template` SET `display_id1` = 1115, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 2654;
+
+        -- Vilebranch Warrior
+        UPDATE `creature_template` SET `display_id1` = 1118, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 4465;
+
+        -- Vilebranch Scalper
+        UPDATE `creature_template` SET `display_id1` = 1111, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 4466;
+
+        -- Vilebranch Soothsayer
+        UPDATE `creature_template` SET `display_id1` = 1115, `display_id2` = 0, `display_id3` = 0, `display_id4` = 0 where `entry` = 4467;
+
+        -- Jade Sludge
+        UPDATE `creature_template` SET `display_id1` = 1146 where `entry` = 4468;
+
+        insert into`applied_updates`values ('290320233');
+    end if;
+
+    -- 29/03/2023 4
+    if (select count(*) from `applied_updates` where id='290320234') = 0 then
+
+        -- Defias Renegade Mage
+        UPDATE `creature_template` SET `display_id1` = 263, `display_id2` = 278, `display_id3` = 0, `display_id4` = 0 where `entry` = 450;
+
+        insert into`applied_updates`values ('290320234');
     end if;
 
 end $
