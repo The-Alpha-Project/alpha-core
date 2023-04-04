5bfeed9e931a93120eaf8357fece7e8c662783c4
Richard Kämmerer <richard@richardkaemmerer.de>
2023-04-04 20:05:29 +0200

    Quest and object fixes (#1116)
    * Fix Ratchet/Tanaris goblin faction
    
    * Despawn objects, fix quests
    
    * Fix for Echeyakee quest text, move quest to different NPC
    
    * More objects
    
    * More objects
    
    * Fix quest The Angry Scytheclaws
    
    * Despawn a non-existant camp
    
    * Despawn more objects, fix quest Deepmoss Spider Eggs
    
    * Fix Z of two Sepulcher NPC
    
    * Fix Barrel of Milk
    
    * Fix duplicate applied_updates
    
    * Restore quest Goblin Invaders
    
    * Remove invalid display_ids from Southsea pirates
    
    * Fix Theramore mobs in Barrens
    
    * Spawn Cannoneers Smythe & Wesson
    
    * Move Kodo
    
    * Despawn Tool Bucket
    
    * Despawn Gallywix' Lockbox
    
    * Despawn misplaced objects/creatures
    
    * Despawn objects without displayId that have no use or are misplaced
    
    * More objects
    
    * More objects
    
    * Remaining objects
    
    * Fix faction for Wizzlecranks Shredder
    
    * More objects
    
    * Fixes for several pet trainers
    
    * Rewrite quest Blueleaf Tubers
    
    * Remove Snufflenose Owner's Manual, its not asked for
    
    * Restore and move quest Shards of a God
    
    * Restore original quest details text for Weapons of Choice
    
    * Another GO to despawn
    
    * Closes #1106
    
    * Fix for Jezelle's Imp and Succubus
    
    * Fix Helena Atwood's OOC script (used NYI spell id)
    
    * Remove blank lines
    
    * Closes 1055
    
    * Fix spacing
    
    * Fix typo
    
    * Leave Snufflenose Owner's Manual in quest req's
    
    * Don't spawn Skeletal Enforcer as alternate
    
    * Close #1117
    
    * Despawn Echeyakee's Lair spell focus
    
    ---------
    
    Co-authored-by: Richard Kämmerer <richard.kaemmerer@gmail.com>

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1,8 +1,8 @@
 delimiter $
 begin not atomic
 
-    -- 27/03/2023 1
-    if(select count(*) from applied_updates where id = '270320231') = 0 then
+    -- 27/03/2023 2
+    if(select count(*) from applied_updates where id = '270320232') = 0 then
         -- Set subname for Brock Stoneseeker
         update creature_template set subname = "Cartography Trainer" where entry = 1681;
         -- Set subname for Karm Ironquill
@@ -31,7 +31,7 @@ begin not atomic
         -- set proper faction and have him spawn his spider pet
         update creature_template set faction = 57, spell_id1 = 7912 where entry = 2872;
 
-        insert into applied_updates values ('270320231');
+        insert into applied_updates values ('270320232');
     end if;
 
     -- 29/03/2023 1
@@ -254,6 +254,148 @@ begin not atomic
         insert into`applied_updates`values ('300320231');
     end if;
 
+    -- 31/03/2023 2
+    if (select count(*) from `applied_updates` where id='310320232') = 0 then
+
+        -- Fix Ratchet/Tanaris faction templates by aligning all these goblins with Booty Bay
+        update creature_template set faction = 121 where faction in(474, 637);
+
+        -- Despawn objects from Barrens affected by vanilla terrain changes
+        update spawns_gameobjects set ignored = 1 where spawn_id in(13241, 13505, 13320, 12935, 2183, 13202, 1814, 2066, 15688, 2725, 1901, 2160, 13314, 2501, 1992);
+
+        -- Fix quest Verog the Dervish - he's not near any command tents since there is no command tent yet.
+        update quest_template set Details = "The centaur Verog the Dervish wanders the Barrens, and will be difficult to find. But he is based at the camps near the Stagnant Oasis to the southeast. It may be possible to draw him to you.$B$BTravel to the centaur camps near the Stagnant Oasis and attack the centaurs there. It will be dangerous, but if you can kill enough centaurs at those camps then they should raise an alarm. And Verog will come.$B$BBring me his head and I will place it with Barak Kodobane's." where entry = 851;
+
+        -- Fix quest Echeyakee - he's not summoned by any horn, nor does the horn item even exist
+        -- Also fix Objectives to mention the changed questgiver (see below)
+        update quest_template set Objectives = "Bring Echeyakee's Hide to Jorn Skyseer at Camp Taujaro.", Details = "Whitemist, Echeyakee in our tongue, is the king of the savannah cats. With such steath does he hunt, he is like a thin white mist on the earth. And with such speed does he kill, his prey have no time for fear, or pain.$B$BHe is mercy, and he is death.$B$BYou will learn this, for I set you on the path to hunt Echeyakee. He stalks with his lion brethren, northeast of The Crossroads...$B$BGo. He is waiting.", OfferRewardText = "You have beaten Echeyakee, and though he hunts no more... his spirit is with you. He will show you the strength found in subtlety, and the honor in mercy.$B$BYour path is long, young $c. Stride it well." where entry = 881;
+
+        -- Spawn Echeyakee at retail position of his lair - it's northeast of the Crossroads and with his "lion brethren"
+        insert into spawns_creatures (spawn_entry1, map, position_x, position_y, position_z, orientation, spawntimesecsmin, spawntimesecsmax, movement_type) values (3475, 1, 458.11, -3035.91, 91.6839, 0, 300, 300, 1);
+
+        -- Move quest Echeyakee to Jorn Skyseer in Camp Taujaro. Evidence suggests he was originally the quest starter, not Sergra Darkthorn.
+        update creature_quest_starter set entry = 3387 where quest = 881;
+        update creature_quest_finisher set entry = 3387 where quest = 881;
+
+        -- Fix quest text of The Angry Scytheclaws
+        update quest_template set Details = "The Sunscale Scytheclaws have gone berserk. Normally they fight with Savannah Prowlers for food, keeping both predators scarce.$B$BWithout their natural enemy, they have started stalking the roads, picking off unwary travelers. You must thin their numbers, $N.$B$BFind the Sunscale Scytheclaws to the south, slay them and cut out their bladders. Use the Scytheclaw Bladders to defile the Scytheclaw nests, southwest of the Stagnant Oasis. With their nests defiled they will abandon them and move elsewhere.", RequestItemsText = "Is your task finished? Ponder the life of the Scytheclaw as you do it. There are important lessons within every creature's lifespawn.", OfferRewardText = "Some of the others believe I have been too heavy handed in my lesson. I know that you are simply following my orders, but I want you to consider the life of the creatures you are slaughtering.$B$BThough they are at times a nuisance, they only become threatening when we seek their slaughter. This days defilement will cause us more trouble than it will solve problems...", Objectives = "Kill Sunscale raptors and collect their bladders. Use the bladders on the 3 Scytheclaw nests. Return to Sergra Darkthorn in the Crossroads." where entry = 905;
+
+        -- Despawn Horde Guards from a non-existant camp
+        update spawns_creatures set ignored = 1 where spawn_id in(19402, 19361, 19326, 19438, 19412);
+        -- Despawn shane cubed benches from the same camp
+        update spawns_gameobjects set ignored = 1 where spawn_id in(13407, 13403);
+
+        -- Fix quest text of Deepmoss Spider Eggs
+        update quest_template set Details = "Spider egg omelettes are a new fad in Booty Bay! The problem is... Booty Bay's got no supply of eggs.$B$BI smell an opportunity!$B$BIn Windshear Crag -- in the Stonetalon Mountains to the northwest -- lives the deepmoss spider. Bring me its eggs and I'll pay a bundle!$B$BThe spiders like to creep under the shade of trees... Too bad the Venture Company cut down most of their trees!$B$BBut go to Windshear Crag anyway and look for deepmoss spiders. Their eggs will be clustered under what trees remain." where entry = 1069;
+
+        -- Fix Z of two NPCs in Sepulcher
+        update spawns_creatures set position_z = 104.877 where spawn_id in(17622, 17625);
+
+        -- Fix Z for Barrel of Milk in Stonetalon Mountains
+        update spawns_gameobjects set spawn_positionZ = 22.9 where spawn_id = 47559;
+
+        -- Restore quest Goblin Invaders
+        update quest_template set Objectives = "Kill 12 Venture Co. Cutters, 10 Venture Co. Grinders and 6 Venture Co. Loggers, then return to Seereth Stonebreak on the border of Stonetalon and the Barrens.", ReqCreatureOrGOId3 = 3989, ReqCreatureOrGOCount3 = 6 where entry = 1062;
+
+        -- Despawn a floating book in Ratchet, table is missing
+        update spawns_gameobjects set ignored = 1 where spawn_id = 13452;
+
+        -- Remove invalid display_id from Southsea Cannoneer
+        update creature_template set display_id2 = 3828, display_id3 = 0 where entry = 3382;
+        -- Same for Southsea Brigand
+        update creature_template set display_id4 = 0 where entry = 3381;
+        -- And for Southsea Cutthroat
+        update creature_template set display_id2 = 3836, display_id3 = 0, display_id4 = 0 where entry = 3383;
+
+        -- Set proper faction for Theramore units in The Barrens - they need to be hostile to Horde
+        update creature_template set faction = 150 where entry in(3454, 3455, 3393, 3385, 3386);
+
+        -- Despawn a bunch of objects from not-yet-existant Northwatch Hold
+        update spawns_gameobjects set ignored = 1 where spawn_id in(13360, 12705, 13365, 13354);
+
+        -- Spawn Cannoneers Smythe & Wesson
+        update spawns_creatures set ignored = 0, position_x = -2088.116, position_y = -3683.056, position_z = 50.397, orientation = 4.450 where spawn_id = 12165;
+        update spawns_creatures set ignored = 0, position_x = -2097.684 , position_y = -3675.315, position_z = 50.254, orientation = 4.450 where spawn_id = 12166;
+
+        -- Slightly move a Lost Barrens Kodo spawn as it spawns inside a house
+        update spawns_creatures set position_x = 753.196, position_y = -2777.454, position_z = 92.003 where spawn_id = 15119;
+
+        -- Despawn unused object Tool Bucket from Barrens
+        update spawns_gameobjects set ignored = 1 where spawn_entry = 161752;
+
+        -- Despawn unused object Gallywix' Lockbox and trap object
+        update spawns_gameobjects set ignored = 1 where spawn_entry in(129127, 130126);
+
+        -- Despawn Venture Co. Drudgers spawning under water
+        update spawns_creatures set ignored = 1 where spawn_id in(20791, 20784);
+
+        -- Despawn shane cubed benches
+        update spawns_gameobjects set ignored = 1 where spawn_id in(13396, 13392);
+
+        -- Despawn various shane cubed objects that serve no purpose in 0.5.3
+        update spawns_gameobjects set ignored = 1 where spawn_id in(13531, 20389, 10808, 12353, 14685, 17202, 17198, 17203, 17204, 17205, 40668, 47699, 32611, 14106, 20637, 1712, 1657, 16873, 3996126, 28294, 12863, 12872, 12864, 15175, 15176, 40694, 42107, 42108, 42109, 14411, 14412, 14422, 33253, 33221, 14441, 14393, 14394, 47966, 47968, 49828, 10919, 40696, 40698, 17228, 18013, 50016, 399472, 230924, 6910, 50158, 99847, 11389, 6447, 17633, 7024, 11922, 32045, 399323, 48357, 48460, 42112, 4523, 98643, 17972, 6942, 13500, 17428, 17598, 17599, 17600, 17926, 17500, 2, 33240, 5258, 6813, 6936, 17597, 17745);
+        update spawns_gameobjects set ignored = 1 where spawn_entry in(89634, 90566, 90858, 102985, 126052, 126053, 141812, 141853, 141857, 141858, 141859, 141931, 142175, 142176, 142208, 142209, 142210, 142211, 142212, 142213, 142344, 142345, 142475, 142476, 142487, 142488, 142696, 143979, 147516, 147517, 148418, 148419, 148420, 148421, 148503, 148837, 148883, 148937, 124370, 149432, 149433, 150140, 153204, 153205, 157816, 157817, 157818, 157819, 157820, 161527, 162024, 164870, 175084, 175085, 175207, 175226, 175227, 175233, 175246, 175247, 175248, 175249, 175324, 175329, 175330, 175331, 175490, 175491, 175492, 175586, 175524, 175587, 175787, 175788, 175889, 175890, 175891, 175894, 175924, 175925, 175926, 175928, 175933, 175659, 175944, 175948, 176157, 176165, 176188, 176190, 176196, 176197, 176198, 176208, 176591, 176209, 176865, 176210, 176213, 176214, 176306, 176361, 176393, 176392, 176582, 176635, 176745, 176747, 176967, 176968, 176969, 176970, 176971, 176972, 176973, 176987, 176988, 176989, 176990, 176991, 176992, 176993, 176994, 177045, 177185, 177188, 177198, 177226, 177227, 177243, 177249, 177250, 177251, 177252, 177253, 177254, 177255, 177256, 177264, 177271, 177289, 177307, 177365, 177366, 177369, 177397, 177398, 177399, 177400, 177484, 177485, 177644, 177667, 177677, 177747, 177785, 177787, 177791, 177794, 177793, 177804, 177805, 177806, 177884, 177927, 177964, 178087, 178089, 178125, 178144, 178146, 178147, 178195, 178247, 178324, 178325, 178386, 178553, 178571, 178934, 179144, 179224, 179469, 179736, 179827, 179879, 180100, 180104, 180323, 180335, 180811, 180866, 186218, 68865, 164651);
+        -- Set placeholders for Azsharite Formation
+        update gameobject_template set displayId = 219 where entry in(152620, 152621, 152631);
+
+        -- Fix faction for Wizzlecrank's Shredder
+        update creature_template set faction = 121 where entry = 3439;
+
+        -- Fix Ransin Donner to actually be a Crab Trainer
+        update creature_template set faction = 11, trainer_type = 3, trainer_class = 3, trainer_id = 282, spell_id1 = 7907 where entry = 2943;
+        -- Fix Kurll to be a Cat Trainer
+        update creature_template set trainer_type = 3, trainer_class = 3, spell_id1 = 7906, trainer_id = 277 where entry = 3621;
+        -- Fix Rarck to be a Raptor Trainer
+        update creature_template set trainer_type = 3, trainer_class = 3, spell_id1 = 7910, trainer_id = 285, faction = 85 where entry = 3625;
+        -- Fix Harb Clawhoof to be a Cat Trainer
+        update creature_template set trainer_type = 3, trainer_class = 3, trainer_id = 277 where entry = 3685;
+        -- Fix Alanndrian Nightsong to be a Tallstrider Trainer
+        update creature_template set trainer_type = 3, trainer_class = 3, trainer_id = 286, spell_id1 = 7913 where entry = 3702;
+        -- Fix Galthuk to be a Bear Trainer
+        update creature_template set trainer_type = 3, trainer_class = 3, trainer_id = 278, spell_id1 = 7903 where entry = 4043;
+
+        -- Rewrite quest Blueleaf Tubers to it's original form
+        -- Proof: https://web.archive.org/web/20040908182039/http://www.goblinworkshop.com/quests/redleaf-tubers.html
+        update quest_template set Title = "Redleaf Tubers", Details = "Redleaf tubers are a delicacy around the world! But they're rare, very rare. The only place to find them is here in the Barrens, deep in the earth, in Razorfen Kraul.$B$BAnd even then, they're impossible to find unless you know just where to look! That's why I've trained these snufflenose gophers to find them for me. They have great noses and can smell a tuber from fifty paces away.$B$BIt won't be easy, but if you get me some tubers I'll pay you handsomely.", Objectives = "Grab a Crate with Holes. Grab a Snufflenose Command Stick. Grab and read the Snufflenose Owner's Manual. In Razorfen Kraul, use the Crate with Holes to summon a Snufflenose Gopher, and use the Command Stick on the gopher to make it search for Tubers. Bring 10 Redleaf Tubers, the Snufflenose Command Stick and the Crate with Holes to Mebok Mizzyrix in Ratchet." where entry = 1221;
+        -- Rename Blueleaf Tubers object to Redleaf Tubers
+        update gameobject_template set name = "Redleaf Tuber" where entry = 20920;
+
+        -- Restore quest Shards of a God (replaces Spirits of the Wind)
+        update quest_template set Title = "Shards of a God", SpecialFlags = 0, PrevQuestId = 0, Details = "When the vines came out of the earth, they often forced veins of minerals to surface also.$B$BOne special mineral found near the vines has been named blood ore. The Bristleback tribe think the blood ore is sacred and part of Agammagan's body. They sometimes carry shards of it for good fortune.$B$BI think I may be able to create a new alloy from the ore if I were to get enough of it. Find the Bristleback to the south of here, across the dry riverbed, and bring me back as many blood shards as you can.", Objectives = "Bring 20 Blood Shards to Tatternack Steelforge at the Crossroads.", ReqItemCount1 = 20, RewOrReqMoney = 854, RewSpell = 0, RewSpellCast = 0, OfferRewardText = "", RequestItemsText = "" where entry = 889;
+        -- Move quest Shards of a God to Tatternack Steelforge
+        update creature_quest_starter set entry = 3433 where quest = 889;
+        update creature_quest_finisher set entry = 3433 where quest = 889;
+
+        -- Restore original quest details of Weapons of Choice
+        update quest_template set Details = "The warchief has instructed me to study all kinds of weapons and armor. He thinks there's something to be learned from even the most pathetic of cultures.$B$BTake the Razormane quillboars to the south, beyond the Field of Giants. As far as I know, they have no skilled blacksmiths, but I'm told they've started to develop sturdier weapons. I'd like to get my hands on a few to learn their techniques.$B$BIf you come across some of their rarer items, bring them to me, and you'll bring honor to the Horde." where entry = 893;
+
+        -- Set proper display_id for Watcher Mocarski
+        update creature_template set display_id1 = 2395 where entry = 827;
+
+        -- Set trainer list for Teg Dawnstrider <Enchanting Trainer>
+        update creature_template set trainer_id = 508 where entry = 3011;
+
+        -- Fix display_id for Jezelle's Imp and Succubus
+        update creature_template set display_id1 = 1213 where entry = 5730;
+        update creature_template set display_id1 = 159 where entry = 5728;
+
+        -- Fix Helena Atwood's OOC script (used NYI spell id)
+        update creature_ai_scripts set datalong = 1485 where id = 566901;
+
+        -- Restore Skeletal Enforcer based on level 24-25 Skeletal Fiend
+        update creature_template set name = "Skeletal Enforcer", faction = 21, static_flags = 0, level_min = 24, level_max = 25, health_min = 664, health_max = 712, armor = 1009, dmg_min = 36, dmg_max = 46, attack_power = 106, loot_id = 531, pickpocket_loot_id = 531, gold_min = 33, gold_max = 48, ai_name = 'EventAI', movement_type = 1, mechanic_immune_mask = 8602131 where entry = 725;
+        -- Spawn Skeletal Enforcer as replacement for some Skeletal Fiend and Skeletal Horror on Duskwood Cemetary
+        update spawns_creatures set spawn_entry1 = 725 where spawn_id in(4941, 5160, 5040, 4944);
+        update spawns_creatures set spawn_entry1 = 725 where spawn_id in(5178, 4993, 4974, 5942);
+
+        -- Set Lord Daval display_id
+        update creature_template set display_id1 = 2039 where entry = 1749;
+        -- Revert Scarlet Mage display_id to placeholder
+        update creature_template set display_id1 = 1640, display_id2 = 1641 where entry = 1832;
+
+        insert into`applied_updates`values ('310320232');
+    end if;
+
     -- 31/03/2023 1
     if (select count(*) from `applied_updates` where id='310320231') = 0 then
 
@@ -301,14 +443,14 @@ begin not atomic
 
         insert into`applied_updates`values ('310320231');
     end if;
-        
+
     -- 01/04/2023 1
     if (select count(*) from `applied_updates` where id='010420231') = 0 then
         UPDATE `quest_template` SET `NextQuestId` = '1091', `ExclusiveGroup` = '-1079' WHERE (`entry` = '1079');
         UPDATE `quest_template` SET `PrevQuestId` = '0', `NextQuestId` = '1091', `ExclusiveGroup` = '-1079' WHERE (`entry` = '1080');
         UPDATE `gameobject_template` SET `data0` = '0' WHERE (`entry` = '24776');
         UPDATE `item_template` SET `spellid_1` = '0' WHERE (`entry` = '6145');
-        
+
         insert into`applied_updates`values ('010420231');
     end if;
 end $
