c283f05f807188bb53f4ec5080ed826ec6ee7e9d
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-04-04 11:53:51 +0200

    Closes #1106

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -369,6 +369,9 @@ begin not atomic
         -- Restore original quest details of Weapons of Choice
         update quest_template set Details = "The warchief has instructed me to study all kinds of weapons and armor. He thinks there's something to be learned from even the most pathetic of cultures.$B$BTake the Razormane quillboars to the south, beyond the Field of Giants. As far as I know, they have no skilled blacksmiths, but I'm told they've started to develop sturdier weapons. I'd like to get my hands on a few to learn their techniques.$B$BIf you come across some of their rarer items, bring them to me, and you'll bring honor to the Horde." where entry = 893;
 
+        -- Set proper display_id for Watcher Mocarski
+        update creature_template set display_id1 = 2395 where entry = 827;
+
         insert into`applied_updates`values ('310320232');
     end if;
 
