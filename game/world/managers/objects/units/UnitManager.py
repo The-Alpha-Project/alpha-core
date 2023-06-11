cbc75be139f573fcc54471b565c564475c1b93e3
devwar <108442943+devw4r@users.noreply.github.com>
2023-06-10 19:59:02 -0600

    Update UnitManager.py

--- game/world/managers/objects/units/UnitManager.py
+++ game/world/managers/objects/units/UnitManager.py
@@ -440,6 +440,10 @@ class UnitManager(ObjectManager):
         if self.location.has_in_arc(attacker.location, math.pi):
             return
 
+        # Not chasing, ignore.
+        if not self.is_moving():
+            return
+
         # Check if already dazed.
         if self.aura_manager.has_aura_by_spell_id(1604):
             return
