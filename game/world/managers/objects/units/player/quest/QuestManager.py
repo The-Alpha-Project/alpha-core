7c6ca780fc829f16e8c4f33d301f4678f31f7a41
devwar <108442943+devw4r@users.noreply.github.com>
2023-06-18 16:49:27 -0600

    Update QuestManager.py

--- game/world/managers/objects/units/player/quest/QuestManager.py
+++ game/world/managers/objects/units/player/quest/QuestManager.py
@@ -914,9 +914,9 @@ class QuestManager(object):
         # Check chosen reward item.
         reward_items = {}
         rew_item_choice_list = list(filter((0).__ne__, QuestHelpers.generate_rew_choice_item_list(quest)))
-
+        rew_item_choice_count = list(filter((0).__ne__, QuestHelpers.generate_rew_choice_count_list(quest)))
         if item_choice < len(rew_item_choice_list) and rew_item_choice_list[item_choice] > 0:
-            reward_items[rew_item_choice_list[item_choice]] = 1
+            reward_items[rew_item_choice_list[item_choice]] = rew_item_choice_count[item_choice]
 
         # Check not chosen reward item(s).
         rew_item_list = list(filter((0).__ne__, QuestHelpers.generate_rew_item_list(quest)))
