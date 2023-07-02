aca9d6295e57822d0f69990f8fdaf7f1153f090a
Fluglow <38987331+Fluglow@users.noreply.github.com>
2023-07-02 17:54:03 +0300

    Fix melee abilities not triggering aura procs

--- game/world/managers/objects/units/UnitManager.py
+++ game/world/managers/objects/units/UnitManager.py
@@ -839,6 +839,10 @@ class UnitManager(ObjectManager):
         damage_info = self.calculate_spell_damage(damage, miss_reason, hit_flags, spell_effect, target)
 
         is_cast_on_swing = spell.casts_on_swing()
+
+        if is_cast_on_swing:
+            self.handle_melee_attack_procs(damage_info)
+
         # TODO Should other spells give skill too?
         if is_cast_on_swing or spell.is_ranged_weapon_attack():
             self.handle_combat_skill_gain(damage_info)
