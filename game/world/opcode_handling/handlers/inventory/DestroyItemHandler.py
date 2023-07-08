0066a92c211da7b49cdb308937f19d6f5e9d10d4
devwar <108442943+devw4r@users.noreply.github.com>
2023-07-07 22:18:36 -0600

    Refactor.
    - Update quest surrounding upon

--- game/world/opcode_handling/handlers/inventory/DestroyItemHandler.py
+++ game/world/opcode_handling/handlers/inventory/DestroyItemHandler.py
@@ -1,4 +1,6 @@
 from struct import unpack
+
+from game.world.opcode_handling.HandlerValidator import HandlerValidator
 from utils.constants.ItemCodes import InventoryError, InventorySlots
 
 
@@ -6,22 +8,26 @@ class DestroyItemHandler(object):
 
     @staticmethod
     def handle(world_session, reader):
+        # Validate world session.
+        player_mgr, res = HandlerValidator.validate_session(world_session, reader.opcode, disconnect=True)
+        if not player_mgr:
+            return res
+
         if len(reader.data) >= 3:  # Avoid handling empty destroy item packet.
             bag, source_slot, count = unpack('<3B', reader.data[:3])
 
             if bag == 0xFF:
                 bag = InventorySlots.SLOT_INBACKPACK.value
 
-            item = world_session.player_mgr.inventory.get_item(bag, source_slot)
+            item = player_mgr.inventory.get_item(bag, source_slot)
             if not item:
                 return 0
 
             if item.is_container() and not item.is_empty():
-                world_session.player_mgr.inventory.send_equip_error(
-                    InventoryError.BAG_NOT_EMPTY, item)
+                player_mgr.inventory.send_equip_error(InventoryError.BAG_NOT_EMPTY, item)
                 return 0
 
-            world_session.player_mgr.inventory.remove_item(bag, source_slot, True)
-            world_session.player_mgr.quest_manager.update_surrounding_quest_status()
+            player_mgr.inventory.remove_item(bag, source_slot, clear_slot=True)
+            player_mgr.quest_manager.update_surrounding_quest_status()
 
         return 0
--- game/world/opcode_handling/handlers/npc/BuyBankSlotHandler.py
+++ game/world/opcode_handling/handlers/npc/BuyBankSlotHandler.py
@@ -2,19 +2,25 @@ from struct import unpack
 
 from database.dbc.DbcDatabaseManager import DbcDatabaseManager
 from game.world.managers.maps.MapManager import MapManager
+from game.world.opcode_handling.HandlerValidator import HandlerValidator
 
 
 class BuyBankSlotHandler(object):
 
     @staticmethod
     def handle(world_session, reader):
+        # Validate world session.
+        player_mgr, res = HandlerValidator.validate_session(world_session, reader.opcode, disconnect=True)
+        if not player_mgr:
+            return res
+
         if len(reader.data) >= 8:  # Avoid handling empty buy bank slot packet.
             guid = unpack('<Q', reader.data[:8])[0]
-            banker = MapManager.get_surrounding_unit_by_guid(world_session.player_mgr, guid)
+            banker = MapManager.get_surrounding_unit_by_guid(player_mgr, guid)
             if banker:
-                next_slot = world_session.player_mgr.player.bankslots + 1
+                next_slot = player_mgr.player.bankslots + 1
                 slot_cost = DbcDatabaseManager.bank_get_slot_cost(next_slot)
                 # Check if player can afford it (even if client already checks it)
-                if world_session.player_mgr.coinage >= slot_cost:
-                    world_session.player_mgr.add_bank_slot(slot_cost)
+                if player_mgr.coinage >= slot_cost:
+                    player_mgr.add_bank_slot(slot_cost)
         return 0
