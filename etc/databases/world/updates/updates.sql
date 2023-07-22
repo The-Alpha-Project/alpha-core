e149e138ca90f9ba72a133a2598b0ed139b86846
devw4r <108442943+devw4r@users.noreply.github.com>
2023-07-22 13:05:18 -0600

    Hin Denburg, Torc the Orc.
    - Correct npc text for both.
    - Add Journey To Orgrimmar to Hin Denburg

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -3002,5 +3002,20 @@ begin not atomic
 
         insert into`applied_updates`values ('180720231');
     end if;
+
+    -- 22/07/2023 1
+    if (select count(*) from `applied_updates` where id='220720231') = 0 then
+        -- Hin Denburg npc text.
+        UPDATE `npc_text` SET `text0_0` = 'My zeppelin is the sturdiest vessel in the land. And if I have a any more grog tonight it will be the sturdiest vessel in the sea!' WHERE (`id` = '2753');
+
+        -- Hin Denburg Journey To Orgrimmar
+        INSERT INTO `creature_quest_starter` (`entry`, `quest`) VALUES ('3150', '799');
+        INSERT INTO `creature_quest_finisher` (`entry`, `quest`) VALUES ('3150', '799');
+
+        -- Torc the Orc
+        INSERT INTO `alpha_world`.`npc_text` (`id`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `lang1`, `prob1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `lang2`, `prob2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `lang3`, `prob3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `lang4`, `prob4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `lang5`, `prob5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `lang6`, `prob6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `lang7`, `prob7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`) VALUES ('3148', 'I am Torc the Orc! Where would you like to fly today?', '', '0', '1', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0', '', '', '0', '0', '0', '0', '0', '0', '0', '0');
+
+        insert into`applied_updates`values ('220720231');
+    end if;
 end $
 delimiter ;
