d6f7e9322ee9052d880be36f5d637cf78525a888
devwar <108442943+devw4r@users.noreply.github.com>
2023-08-02 00:07:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ parser.add_argument(
 )
 parser.add_argument(
     '-e', '--extract',
-    help='-e maps in order to extract .map files',
+    help='-e in order to extract .map files',
     dest='extract',
     action='store_true',
     default=False
