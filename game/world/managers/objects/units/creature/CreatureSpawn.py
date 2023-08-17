ffb1d6d560033af48f1b833f411e9c2df5c0321b
geo <tp53.geo@gmail.com>
2023-08-17 06:31:29 +0200

    implement Pooling system in CreatureSpawn

--- game/world/managers/objects/units/creature/CreatureSpawn.py
+++ game/world/managers/objects/units/creature/CreatureSpawn.py
@@ -1,13 +1,14 @@
-from random import choice, randint
+from random import choice, choices, randint
 from typing import Optional
 
 from database.world.WorldModels import SpawnsCreatures
+from database.world.WorldDatabaseManager import WorldDatabaseManager
 from game.world.managers.abstractions.Vector import Vector
 from game.world.managers.maps.MapManager import MapManager
+from game.world.managers.maps.PoolHolder import PoolHolder
 from game.world.managers.objects.units.creature.CreatureBuilder import CreatureBuilder
 from game.world.managers.objects.units.creature.CreatureManager import CreatureManager
 from utils.Logger import Logger
-
 
 class CreatureSpawn:
     def __init__(self, creature_spawn, instance_id):
@@ -26,6 +27,7 @@ class CreatureSpawn:
         self.respawn_time = 0
         self.last_tick = 0
         self.borrowed = False
+        self.pool_entry = None
 
     def update(self, now):
         if now > self.last_tick > 0:
@@ -71,21 +73,46 @@ class CreatureSpawn:
                            f'Spawn id:{self.creature_spawn.spawn_id}. ')
             return False
 
-        creature_location = self.get_default_location()
-        self.respawn_timer = 0
-        self.respawn_time = randint(self.creature_spawn.spawntimesecsmin, self.creature_spawn.spawntimesecsmax)
-        self.creature_instance = CreatureBuilder.create(creature_template_id, creature_location,
-                                                        self.map_id, self.instance_id,
-                                                        health_percent=self.health_percent,
-                                                        mana_percent=self.mana_percent,
-                                                        addon=self.addon,
-                                                        wander_distance=self.creature_spawn.wander_distance,
-                                                        movement_type=self.creature_spawn.movement_type,
-                                                        spawn_id=self.spawn_id)
+        pool_creature, pool_entry = PoolHolder.get_chosen_pool_creature_spawn(self.spawn_id)
+        spawned_pool_creatures = None
 
-        self.creature_instance.get_map().spawn_object(world_object_spawn=self,
-                                                      world_object_instance=self.creature_instance)
-        return True
+        if pool_creature:
+            self._update(pool_creature, pool_entry)
+            spawned_pool_creatures = PoolHolder.get_creature_spawn_ids_by_pool_entry(pool_entry)                
+
+        if not spawned_pool_creatures:
+            self.respawn_timer = 0
+            self.respawn_time = randint(self.creature_spawn.spawntimesecsmin, self.creature_spawn.spawntimesecsmax)
+            self.creature_instance = CreatureBuilder.create(creature_template_id, self.location,
+                                                            self.map_id, self.instance_id,
+                                                            health_percent=self.health_percent,
+                                                            mana_percent=self.mana_percent,
+                                                            addon=self.addon,
+                                                            wander_distance=self.wander_distance,
+                                                            movement_type=self.movement_type,
+                                                            spawn_id=self.spawn_id)
+
+            self.creature_instance.get_map().spawn_object(world_object_spawn=self,
+                                                        world_object_instance=self.creature_instance)
+
+            if pool_creature:
+                PoolHolder.add_active_creature_spawn(pool_entry, self.spawn_id)
+
+            return True
+
+        return False
+
+    def _update(self, creature, pool_entry):
+        self.creature_spawn: SpawnsCreatures = creature
+        self.spawn_id = creature.spawn_id
+        self.movement_type = creature.movement_type
+        self.wander_distance = creature.wander_distance
+        self.health_percent = creature.health_percent
+        self.mana_percent = creature.mana_percent
+        self.map_id = creature.map
+        self.location = self.get_default_location()
+        self.addon = creature.addon
+        self.pool_entry = pool_entry
 
     def _update_respawn(self, elapsed):
         self.respawn_timer += elapsed
@@ -98,6 +125,8 @@ class CreatureSpawn:
 
         # Spawn a new creature instance when needed.
         if self.respawn_timer >= self.respawn_time:
+            if self.pool_entry:
+                PoolHolder.remove_active_creature_spawn(self.pool_entry, self.spawn_id)
             self.spawn_creature()
 
     def get_default_location(self):
