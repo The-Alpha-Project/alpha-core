ce45c4071968896bf0d94608da4ae0a193b75567
Magnus Pettersson <magnus@entropy.nu>
2024-03-18 12:58:58 +0100

    updated update script

--- database-update.sh
+++ database-update.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
 function init_database() {
-    SQL_DUMP=${SQL_DUMP}$(cat etc/databases/${1}/updates/*.sql)
-    echo $SQL_DUMP
-
+    docker cp etc/databases/${1}/updates/updates.sql alpha-core-sql:/tmp/updates.sql
+    docker exec -it alpha-core-sql sh -c "mariadb -u root -ppwd alpha_${1} < /tmp/updates.sql"
+    echo "Done with ${1}"
 }
 
 type="default"
-databases=()
-
-while getopts ":u" opt; do
-  case $opt in
-    u)
-      echo "Added to databases"
-      echo $opt;
-      databases+=("$OPTARG")
-      ;;
-  esac
-done
-
-echo $databases
-
-if [ -z "$databases" ]; then
-    echo "databases is emtpy"
-    echo $databases
-    databases=('dbc' 'realm' 'world')
-fi
+databases=('dbc' 'realm' 'world')
 
 for database in "${databases[@]}"; do
   echo "init_database" "$database" "$type"
-done
+  init_database $database
+done 
