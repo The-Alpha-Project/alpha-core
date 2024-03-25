ed8a67720e144f2bc70fc8a09343cd812ebf36f8
Magnus Pettersson <magnus@entropy.nu>
2024-03-25 16:39:47 +0100

    more telnet rework

--- modules/telnet/CommandManagerExtended.py
+++ modules/telnet/CommandManagerExtended.py
@@ -10,12 +10,77 @@ from game.world.managers.CommandManager import CommandManager, PLAYER_COMMAND_DE
 from utils.Logger import Logger
 
 
-## TODO: Add more methods for commands, 
-## TODO: Add method to check if user is online
-## TODO: Add method method to check if any player is online
-
-
 class CommandManagerExtended(CommandManager):
+    @staticmethod
+    def _handle_command(command_dict):
+        command = command_dict['command']
+        args = command_dict['args']
+        player = command_dict['player']
+
+        if player and args:
+             world_session = WorldSessionStateHandler.get_session_by_character_name(player) 
+        else:
+            world_session = None
+        
+        Logger.info(f"TEST: {command}")
+        Logger.info(f"TEST: {world_session}")
+
+        if command in TELNET_COMMAND_DEFINITIONS:
+            command_func = TELNET_COMMAND_DEFINITIONS[command][0]
+        else:
+            Logger.error(f'Command not found, type /help for help.')
+            return 0
+        
+        if command_func:
+            try:
+                code, res = command_func(world_session, args, player)
+                if code != 0:
+                    Logger.error(f'Error with <{command}> command: {res}')
+                elif res:
+                    Logger.success(f'{res}')
+            except Exception as e:
+                Logger.error(f'{e}')
+
+
+    @staticmethod
+    def help(world_session=None, args=None, cmd=None):
+        Logger.info(f'Listening server commands') 
+        Logger.info(f'Please use this format: /<command> <player> <args>') 
+        Logger.info(f'You can shwo help text on one command: /help <command>\n') 
+
+        if not cmd:
+            for command in TELNET_COMMAND_DEFINITIONS:
+                Logger.info(f'{command}')
+        else:
+            command = TELNET_COMMAND_DEFINITIONS[cmd]
+            Logger.info(f'{cmd}:')
+            Logger.info(f'{command[1]}')
+
+        return 0, f''
+    
+    @staticmethod
+    def kick(world_session, args, player):
+        if not world_session or not player:
+            return 1, f'Missing session or player'
+
+        return CommandManager.handle_command(world_session, args)
+        # return 0, f'Kicked {player} from session: {world_session}'
+
+    @staticmethod
+    def online(world_session,args, player):
+        world_session = WorldSessionStateHandler.get_world_sessions()
+        
+        Logger.info(f'{world_session}:')
+
+
+        if not world_session:
+            return 1, f'Missing session'
+
+        return 0, f'2 people online in {world_session}'
+        
+
+class CommandManagerExtended_bk(CommandManager):
+    
      # Telnet commands
     
     @staticmethod
@@ -162,10 +227,10 @@ class CommandManagerExtended(CommandManager):
 TELNET_COMMAND_DEFINITIONS = {
     'help': [CommandManagerExtended.help, 'prints this message'],
     'kick': [CommandManagerExtended.kick, 'kick your target from the server'],
-    'level': [CommandManagerExtended.level, 'set your or others level'],
-    'msg': [CommandManagerExtended.msg, 'Send message to player'],
-    'money': [CommandManagerExtended.money, 'shows a list on all people online'],
+   # 'level': [CommandManagerExtended.level, 'set your or others level'],
+   # 'msg': [CommandManagerExtended.msg, 'Send message to player'],
+   # 'money': [CommandManagerExtended.money, 'shows a list on all people online'],
     'online': [CommandManagerExtended.online, 'shows a list on all people online'],
-    'tel': [CommandManager.tel, 'teleport you to a location'],
-    'stel': [CommandManager.stel, 'search for a location where you can teleport']
+   # 'tel': [CommandManager.tel, 'teleport you to a location'],
+   # 'stel': [CommandManager.stel, 'search for a location where you can teleport']
 }
--- modules/telnet/TelnetCommandHandler.py
+++ modules/telnet/TelnetCommandHandler.py
@@ -5,8 +5,36 @@ from game.world.WorldSessionStateHandler import WorldSessionStateHandler
 from modules.telnet.CommandManagerExtended import CommandManagerExtended
 
 class TelnetCommandHandler:
-    
+
     def starts(parent_conn=None):
+        if parent_conn:
+            Logger.set_parent_conn(parent_conn)
+        
+        while True:
+            if parent_conn.poll():
+                str = parent_conn.recv()
+
+                if isinstance(str, bytes):
+                    msg = str.decode('utf-8')
+    
+                    if "/" in msg[0]: 
+                        msg = msg[1:]        
+                    else:
+                        # Logger.error(f'Error with <{msg}>. command is missing')
+                        return 0
+
+                    parts = msg.split(maxsplit=2)
+
+                    command_dict = {
+                        "command": parts[0],
+                        "player": parts[1] if len(parts) > 1 else None,
+                        "args": parts[2] if len(parts) > 2 else None
+                    }   
+
+                    CommandManagerExtended._handle_command(command_dict)   
+
+    
+    def starts_bk(parent_conn=None):
         if parent_conn:
             Logger.set_parent_conn(parent_conn)
         
