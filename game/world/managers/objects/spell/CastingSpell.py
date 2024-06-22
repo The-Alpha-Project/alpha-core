c051b423593fc2388bd10b0a4289cb02d93d8bf5
Fluglow <38987331+Fluglow@users.noreply.github.com>
2024-06-23 00:54:09 +0300

    Fix mana regen disruption triggering from abilities / tradeskills

--- game/world/managers/objects/spell/CastingSpell.py
+++ game/world/managers/objects/spell/CastingSpell.py
@@ -280,6 +280,12 @@ class CastingSpell:
     def is_passive(self):
         return self.spell_entry.Attributes & SpellAttributes.SPELL_ATTR_PASSIVE == SpellAttributes.SPELL_ATTR_PASSIVE
 
+    def is_ability(self):
+        return self.spell_entry.Attributes & SpellAttributes.SPELL_ATTR_IS_ABILITY
+
+    def is_tradeskill(self):
+        return self.spell_entry.Attributes & SpellAttributes.SPELL_ATTR_TRADESPELL
+
     def is_channeled(self):
         return self.spell_entry.AttributesEx & SpellAttributesEx.SPELL_ATTR_EX_CHANNELED
 
@@ -503,8 +509,7 @@ class CastingSpell:
             ranged_delay = self.spell_caster.stat_manager.get_total_stat(UnitStats.RANGED_DELAY)
             cast_time += ranged_delay
 
-        if caster_is_unit and not self.spell_entry.Attributes & (SpellAttributes.SPELL_ATTR_TRADESPELL |
-                                                                 SpellAttributes.SPELL_ATTR_IS_ABILITY):
+        if caster_is_unit and not self.is_ability() and not self.is_tradeskill():
             cast_time = self.spell_caster.stat_manager.apply_bonuses_for_value(cast_time, UnitStats.SPELL_CASTING_SPEED)
 
         return max(0, cast_time)
