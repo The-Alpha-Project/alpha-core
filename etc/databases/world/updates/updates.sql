fc7c2888ccb16d994de415bf432878aa4b6ce72b
GrenderG <grenderg@gmail.com>
2024-07-13 02:28:10 +0200

    Update updates.sql

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1,8 +1,21 @@
 delimiter $
 begin not atomic
-
     -- 12/07/2024 1
-	if (select count(*) from applied_updates where id='120720241') = 0 then
+    if (select count(*) from `applied_updates` where id='120720241') = 0 then
+        -- The Defias Brotherhood: The Defias Traitor should have a delay before starting to run.
+        DELETE FROM `quest_start_scripts` WHERE `id`=155;
+        INSERT INTO `quest_start_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+        (155, 0, 0, 61, 155, 600, 0, 0, 0, 0, 0, 8, 0, 15502, 1019, 15501, 0, 0, 0, 0, 0, 'The Defias Brotherhood: Start Scripted Map Event'),
+        (155, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 'The Defias Brotherhood: The Defias Traitor - Say Text'),
+        (155, 0, 2, 4, 147, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'The Defias Brotherhood: The Defias Traitor - Remove Questgiver Flag'),
+        (155, 3, 3, 25, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'The Defias Brotherhood: The Defias Traitor - Set Run'),
+        (155, 3, 4, 60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'The Defias Brotherhood: The Defias Traitor - Start Waypoints');
+
+        insert into applied_updates values ('120720241');
+    end if;
+    
+    -- 12/07/2024 2
+    if (select count(*) from applied_updates where id='120720242') = 0 then
         -- Green Tea Leaf
         UPDATE `item_template` SET `display_id` = 2336 WHERE (`entry` = 1401);
 
@@ -150,7 +163,7 @@ begin not atomic
         -- Studded Leather Cap
         UPDATE `item_template` SET `display_id` = 3785 WHERE (`entry` = 3890);
 
-        insert into applied_updates values ('120720241');
+        insert into applied_updates values ('120720242');
     end if;
 end $
 delimiter ;
