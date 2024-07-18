0de6f93abbd301201d6a85f5a5a5c828d2a69b40
devw4r <108442943+devw4r@users.noreply.github.com>
2024-07-17 18:13:27 -0600

    More logging to guid error.

--- game/world/opcode_handling/handlers/quest/QuestGiverQueryQuestHandler.py
+++ game/world/opcode_handling/handlers/quest/QuestGiverQueryQuestHandler.py
@@ -28,10 +28,12 @@ class QuestGiverQueryQuestHandler(object):
             if guid in player_mgr.known_objects:
                 quest_giver = player_mgr.known_objects[guid]
 
-            high_guid = GuidUtils.extract_high_guid(guid)
             if not GuidUtils.validate_guid(guid):
-                Logger.error(f'Error in {reader.opcode_str()}, invalid guid: {guid}.')
+                Logger.error(f'Error in {reader.opcode_str()}, invalid guid: {guid}, loc {player_mgr.location}.')
                 return 0
+
+            high_guid = GuidUtils.extract_high_guid(guid)
+
             if high_guid == HighGuid.HIGHGUID_UNIT or high_guid == HighGuid.HIGHGUID_PET:
                 quest_giver = quest_giver if quest_giver else player_mgr.get_map().get_surrounding_unit_by_guid(player_mgr, guid)
                 if not quest_giver:
