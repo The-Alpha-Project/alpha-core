9626624be584f2726e044dd72905b8da93550147
devw4r <108442943+devw4r@users.noreply.github.com>
2024-08-09 13:59:07 -0600

    Update PingHandler.py

--- game/world/opcode_handling/handlers/player/PingHandler.py
+++ game/world/opcode_handling/handlers/player/PingHandler.py
@@ -9,7 +9,8 @@ class PingHandler(object):
     @staticmethod
     def handle(world_session, reader: PacketReader) -> int:
         if len(reader.data) >= 4:  # Avoid handling empty ping packet.
-            world_session.player_mgr.last_ping = time.time()
-            world_session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_PONG, reader.data))
+            if world_session.player_mgr and world_session.player_mgr.online:
+                world_session.player_mgr.last_ping = time.time()
+                world_session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_PONG, reader.data))
 
         return 0
