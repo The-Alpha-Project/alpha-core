0fe4b63c228d1c8aa9aeda66639008fb7939970c
devw4r <108442943+devw4r@users.noreply.github.com>
2024-08-15 14:14:38 -0600

    Update updates.sql

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1460,6 +1460,12 @@ begin not atomic
         UPDATE `playercreateinfo_item` SET `itemid` = '25' WHERE (`id` = '18');
         -- Orc warrior sword.
         UPDATE `playercreateinfo_item` SET `itemid` = '25' WHERE (`id` = '38');
+        
+        -- https://archive.thealphaproject.eu/media/Alpha-Project-Archive/Images/Azeroth/Eastern%20Kingdoms/Hinterlands/AwesomePlaceholder.jpg
+        UPDATE `creature_template` SET `npc_flags` = '10', `trainer_id` = '516', `trainer_type` = '2' WHERE (`entry` = '2688');
+
+        INSERT INTO `trainer_template` (`template_entry`, `spell`, `playerspell`, `spellcost`, `talentpointcost`, `skillpointcost`, `reqskill`, `reqskillvalue`, `reqlevel`) VALUES ('516', '4025', '3971', '0', '0', '0', '202', '200', '0');
+
 
         insert into applied_updates values ('140820241');
     end if;
