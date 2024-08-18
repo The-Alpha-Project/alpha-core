687bd60ee006682c0b7a3b5a2bdc03b0e4cfb387
devw4r <108442943+devw4r@users.noreply.github.com>
2024-08-18 16:57:00 -0600

    Fix pets portrait display.

--- game/world/managers/maps/GridManager.py
+++ game/world/managers/maps/GridManager.py
@@ -121,6 +121,12 @@ class GridManager:
 
         # Notify surrounding players.
         if update_players:
+
+            # Handle pet/guardian player summons, they need to be notified to owner immediately.
+            owner = world_object.get_charmer_or_summoner()
+            if owner and owner.get_type_id() == ObjectTypeIds.ID_PLAYER:
+                owner.update_manager.update_self_temp_summon_creation(world_object)
+
             self._update_players_surroundings(cell.key, object_type=world_object.get_type_id())
 
     def _activate_cell_by_world_object(self, world_object):
