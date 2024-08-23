a3e49e524d57a452f95266320b8143c39d67067f
devw4r <108442943+devw4r@users.noreply.github.com>
2024-08-23 15:17:17 -0600

    Delete PoolTemplate.py

--- game/world/managers/objects/pools/PoolTemplate.py
+++
@@ -1,6 +0,0 @@
-class PoolTemplate:
-    def __init__(self, max_limit, description, flags, instance):
-        self.max_limit = max_limit
-        self.description = description
-        self.flags = flags
-        self.instance = instance
