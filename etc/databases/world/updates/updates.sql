dd41c73fddc28cd7c2f614ded48c343998e81cca
devw4r <108442943+devw4r@users.noreply.github.com>
2024-09-06 13:08:52 -0600

    Update updates.sql

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -158,8 +158,9 @@ begin not atomic
         UPDATE `quest_template` SET `ignored` = '1' WHERE (`entry` = '27');
         -- Ignore quest Finding the Source, invalid items, invalid spells.
         UPDATE `quest_template` SET `ignored` = '1' WHERE (`entry` = '974');
-        -- Enable 'The Great Lift' elevator.
+        -- Enable 'The Great Lift' elevators.
         UPDATE `spawns_gameobjects` SET `ignored` = '0' WHERE (`spawn_id` = '16876');
+        UPDATE `spawns_gameobjects` SET `ignored` = '0' WHERE (`spawn_id` = '16874');
 
         insert into applied_updates values ('040920242');
     end if;
