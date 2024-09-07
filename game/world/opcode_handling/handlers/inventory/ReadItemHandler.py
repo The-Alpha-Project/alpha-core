179986c2f83ce13ac76dd0e3c031acc69d771b05
devw4r <108442943+devw4r@users.noreply.github.com>
2024-09-06 21:49:36 -0600

    CMSG_READ_ITEM

--- game/world/opcode_handling/handlers/inventory/ReadItemHandler.py
+++ game/world/opcode_handling/handlers/inventory/ReadItemHandler.py
@@ -15,12 +15,15 @@ class ReadItemHandler(object):
                 bag = InventorySlots.SLOT_INBACKPACK.value
             item = world_session.player_mgr.inventory.get_item(bag, slot)
 
-            # TODO: Better handling of this: check if player can use item, etc.
+            result = InventoryError.BAG_ITEM_NOT_FOUND
             if item:
+                result = world_session.player_mgr.inventory.can_use_item(item.item_template)
+
+            if result == InventoryError.BAG_OK:
                 data = pack('<2Q', item.guid, item.guid)
                 world_session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_READ_ITEM_OK, data))
             else:
-                world_session.player_mgr.inventory.send_equip_error(InventoryError.BAG_ITEM_NOT_FOUND)
+                world_session.player_mgr.inventory.send_equip_error(result)
                 data = pack('<QIQ', item.guid, 0, item.guid)
                 world_session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_READ_ITEM_FAILED, data))
 
