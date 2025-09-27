cec7e1db86939be600308ab8768897302d6a30fe
devw4r <108442943+devw4r@users.noreply.github.com>
2025-09-27 02:07:49 -0600

    Minor.

--- utils/ByteUtils.py
+++ utils/ByteUtils.py
@@ -2,15 +2,17 @@ class ByteUtils:
     @staticmethod
     def shorts_to_int(short1, short2):
         return (
-            short1 << 16 |
-            short2
+            # Use bitwise AND with 0xFFFF to ensure each short is within 16 bits.
+            ((short1 & 0xFFFF) << 16) |
+            (short2 & 0xFFFF)
         )
 
     @staticmethod
     def bytes_to_int(byte1, byte2, byte3, byte4):
         return (
-            byte1 << 24 |
-            byte2 << 16 |
-            byte3 << 8 |
-            byte4
+            # Use bitwise AND with 0xFF to ensure each byte is within 0-255,
+            (byte1 & 0xFF) << 24 |
+            (byte2 & 0xFF) << 16 |
+            (byte3 & 0xFF) << 8 |
+            (byte4 & 0xFF)
         )
