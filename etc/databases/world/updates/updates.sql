90ef0141bbb59a4f1da55689181a3329539fefe6
GrenderG <grenderg@gmail.com>
2025-10-31 21:27:31 +0100

    Remove wrong quest end script from Skirmish at Echo Ridge. - https://github.com/vmangos/core/commit/4de428f53c2c4481beaa648be04a2c064f44d3e6

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -9,5 +9,16 @@ begin not atomic
         insert into applied_updates values ('311020251');
     end if;
 
+    -- 31/10/2025 2
+    if (select count(*) from applied_updates where id='311020252') = 0 then
+        -- Remove wrong text said at the end of Skirmish at Echo Ridge.
+        -- This text should be said upon accepting Report to Goldshire.
+        -- Proof: https://www.youtube.com/watch?v=SH3HhIsDZ4k&t=373s
+        DELETE FROM `quest_end_scripts` WHERE `id`=21;
+        UPDATE `quest_template` SET `CompleteScript`=0 WHERE `entry`=21;
+
+        insert into applied_updates values ('311020252');
+    end if;
+
 end $
 delimiter ;
