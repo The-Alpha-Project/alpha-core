a783786c129617ba205e5d89ebb1538e134280af
GrenderG <grenderg@gmail.com>
2025-10-31 12:46:28 +0100

    Fix Chickens not being melee attackable.

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -1,5 +1,13 @@
 delimiter $
 begin not atomic
 
+    -- 31/10/2025 1
+    if (select count(*) from applied_updates where id='311020251') = 0 then
+        -- Fix Chickens not being melee attackable.
+        UPDATE creature_template SET npc_flags = 0 WHERE entry = 620;
+
+        insert into applied_updates values ('311020251');
+    end if;
+
 end $
 delimiter ;
