f0d41e5c007ab8407dc052069b993a550a1ee152
GrenderG <grenderg@gmail.com>
2025-11-01 13:04:59 +0100

    Remove buggy escape character on db dumps.

--- etc/databases/realm/realm.sql
+++ etc/databases/realm/realm.sql
@@ -1,4 +1,4 @@
-/*M!999999\- enable the sandbox mode */ 
+/*M!999999- enable the sandbox mode */
 -- MariaDB dump 10.19-11.8.3-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: ar
