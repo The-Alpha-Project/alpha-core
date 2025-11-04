c8140e7dd69e331977fb8062aeb9833ec422d37d
devw4r <108442943+devw4r@users.noreply.github.com>
2025-11-03 19:28:44 -0600

    Update updates.sql

--- etc/databases/world/updates/updates.sql
+++ etc/databases/world/updates/updates.sql
@@ -29,5 +29,31 @@ begin not atomic
         insert into applied_updates values ('021120251');
     end if;
 
+    -- 03/11/2025 1
+    if (select count(*) from applied_updates where id='031120251') = 0 then
+        -- Closes https://github.com/The-Alpha-Project/alpha-core/issues/1503
+        -- Events list for Snufflenose Gopher
+        DELETE FROM `creature_ai_events` WHERE `creature_id`=4781;
+        INSERT INTO `creature_ai_events` (`id`, `creature_id`, `condition_id`, `event_type`, `event_inverse_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action1_script`, `action2_script`, `action3_script`, `comment`) VALUES
+        (478101, 4781, 122101, 1, 0, 40, 1, 2000, 30000, 20000, 30000, 478101, 0, 0, 'Snufflenose');
+
+        DELETE FROM `creature_ai_scripts` WHERE `id`=478101;
+        INSERT INTO `creature_ai_scripts` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `datalong4`, `target_param1`, `target_param2`, `target_type`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
+        (478101, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 1592, 0, 0, 0, 0, 0, 0, 0, 0, 'Snufflenose - Sniff Text'),
+        (478101, 0, 0, 15, 6900, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Snufflenose - Create Tuber');
+
+        DELETE FROM `conditions` WHERE `condition_entry` in (122101, 122102, 122103, 122104);
+        -- 122101: (122102: GameObject 20920 Is Within 60 Yards Of The Target) And (122103:  Not (GameObject 20920 Is Within 5 Yards Of The Target)) And (122104: Friendly Player Within 15 Yards Of The Target)
+        INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (122101, -1, 122102, 122103, 122104, 0, 0);
+        -- 122102: GameObject 20920 Is Within 60 Yards Of The Target
+        INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (122102, 21, 20920, 60, 0, 0, 0);
+        -- 122103:  Not (GameObject 20920 Is Within 5 Yards Of The Target)
+        INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (122103, 21, 20920, 5, 0, 0, 1);
+        -- 122104: Friendly Player Within 15 Yards Of The Target
+        INSERT INTO `conditions` (`condition_entry`, `type`, `value1`, `value2`, `value3`, `value4`, `flags`) VALUES (122104, 56, 2, 15, 0, 0, 0);
+
+        insert into applied_updates values ('031120251');
+    end if;
+
 end $
 delimiter ;
