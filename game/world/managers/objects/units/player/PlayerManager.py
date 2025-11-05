2faf7db5f6bd9456e002743dea8061077e6c67f3
devw4r <108442943+devw4r@users.noreply.github.com>
2025-11-05 16:05:15 -0600

    Update PlayerManager.py

--- game/world/managers/objects/units/player/PlayerManager.py
+++ game/world/managers/objects/units/player/PlayerManager.py
@@ -1431,11 +1431,12 @@ class PlayerManager(UnitManager):
             self.combo_points = min(combo_points + self.combo_points, 5)
 
         self.bytes_2 = self.get_bytes_2()
-        self.set_uint32(UnitFields.UNIT_FIELD_BYTES_2, self.bytes_2)
-
         # Set combo target to a valid but non-existent guid if hiding.
         #  TODO: it's unclear if combo points should be hidden in 0.5.3 for warriors, and if so, how it was done.
-        self.set_uint64(UnitFields.UNIT_FIELD_COMBO_TARGET, self.combo_target if not hide else 0xffffffffffffffff)
+        target_value = self.combo_target if not hide else 0xffffffffffffffff
+
+        self.set_uint64(UnitFields.UNIT_FIELD_COMBO_TARGET, target_value)
+        self.set_uint32(UnitFields.UNIT_FIELD_BYTES_2, self.bytes_2, force=True)
 
     # override
     def remove_combo_points(self):
@@ -1443,10 +1444,11 @@ class PlayerManager(UnitManager):
             return
         self.combo_points = 0
         self.bytes_2 = self.get_bytes_2()
-        self.set_uint32(UnitFields.UNIT_FIELD_BYTES_2, self.bytes_2)
-
         self.combo_target = 0
+
         self.set_uint64(UnitFields.UNIT_FIELD_COMBO_TARGET, self.combo_target)
+        # Force field change on player. https://github.com/The-Alpha-Project/alpha-core/issues/1477
+        self.set_uint32(UnitFields.UNIT_FIELD_BYTES_2, self.bytes_2, force=True)
 
     # override
     def receive_damage(self, damage_info, source=None, casting_spell=None, is_periodic=False):
