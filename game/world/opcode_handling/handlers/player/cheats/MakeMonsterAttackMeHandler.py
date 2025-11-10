24edb93ca99ba1f92dd65aa41e5a9d1f42b8b4b0
devw4r <108442943+devw4r@users.noreply.github.com>
2025-11-09 22:35:15 -0600

    Update MakeMonsterAttackMeHandler.py

--- game/world/opcode_handling/handlers/player/cheats/MakeMonsterAttackMeHandler.py
+++ game/world/opcode_handling/handlers/player/cheats/MakeMonsterAttackMeHandler.py
@@ -17,7 +17,7 @@ class MakeMonsterAttackMeHandler(object):
         if len(reader.data) >= 8:  # Avoid handling empty make monster attack me packet.
             guid = unpack('<Q', reader.data[:8])[0]
             unit = player_mgr.get_map().get_surrounding_unit_by_guid(player_mgr, guid)
-            if unit and unit.is_hostile_to(player_mgr):
+            if unit and (unit.is_hostile_to(player_mgr) or unit.can_attack_target(player_mgr)):
                 unit.attack(player_mgr)
 
         return 0
