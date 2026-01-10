ac9d472b84214688f8abee45f0dedfcd113c3980
devw4r <108442943+devw4r@users.noreply.github.com>
2026-01-10 16:43:53 -0600

    Update SpellManager.py

--- game/world/managers/objects/spell/SpellManager.py
+++ game/world/managers/objects/spell/SpellManager.py
@@ -1821,21 +1821,12 @@ class SpellManager:
         spell_id = casting_spell.spell_entry.ID
         has_error = error != SpellCheckCastResult.SPELL_NO_ERROR
 
-        # Item casts which set the spell as modal (Spell_C_SetModal) will always display the cast result as failed
-        # if the spell id is provided.
-        # if (msg == * (CDataStore **)s_modalSpellID) (msg is spell id, s_modalSpellID is set upon spell cast)
-        #     Spell_C_CancelSpell(0, 0, a1, SPELL_FAILED_ERROR);
-        # This fixes item casts like 5810 (Fresh Carcass) and 5867 (Etched Phial).
-        if casting_spell.source_item and not casting_spell.is_instant_cast():
-            spell_id = 0
-
         if casting_spell.hide_result:
             error = SpellCheckCastResult.SPELL_FAILED_DONT_REPORT
 
         # This fixes item casts like 5810 (Fresh Carcass) and 5867 (Etched Phial).
         # Which are items with no ITEM_FLAG_PLAYERCAST which ends up displaying spell failed in the cast bar.
-        if (not has_error and casting_spell.source_item and not casting_spell.is_instant_cast()
-                and not casting_spell.source_item.is_player_cast()):
+        if not has_error and casting_spell.source_item and not casting_spell.source_item.is_player_cast():
             spell_id = 0
 
         # Send spell failure only if this was an active spell.
