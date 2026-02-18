453259c3d6365e1068109cf84cedc0421d1312b6
devw4r <108442943+devw4r@users.noreply.github.com>
2026-02-17 22:22:41 -0600

    Update ActivateTaxiHandler.py

--- game/world/opcode_handling/handlers/npc/ActivateTaxiHandler.py
+++ game/world/opcode_handling/handlers/npc/ActivateTaxiHandler.py
@@ -1,5 +1,6 @@
 from struct import unpack, pack
 from database.dbc.DbcDatabaseManager import DbcDatabaseManager
+from game.world.opcode_handling.HandlerValidator import HandlerValidator
 from network.packet.PacketWriter import PacketWriter
 from utils.constants.MiscCodes import ActivateTaxiReplies
 from utils.constants.OpCodes import OpCode
@@ -7,31 +8,26 @@ from utils.constants.OpCodes import OpCode
 
 class ActivateTaxiHandler:
     @staticmethod
-    def _is_taxi_blocked_by_shapeshift(player_mgr):
-        if not player_mgr.shapeshift_form:
-            return False
-
-        form_entry = DbcDatabaseManager.spell_shapeshift_form_get_by_id(player_mgr.shapeshift_form)
-        # Stances are shapeshifts in dbc but should not block taxi usage.
-        return not (form_entry and (form_entry.Flags & 1))
-
-    @staticmethod
     def handle(world_session, reader):
+        # Validate world session.
+        player_mgr, res = HandlerValidator.validate_session(world_session, reader.opcode, disconnect=True)
+        if not player_mgr:
+            return res
+
         if len(reader.data) >= 16:  # Avoid handling empty activate taxi packet.
             guid, start_node, dest_node = unpack('<Q2I', reader.data[:16])
-            flight_master = world_session.player_mgr.get_map().get_surrounding_unit_by_guid(
-                world_session.player_mgr, guid)
+            flight_master = player_mgr.get_map().get_surrounding_unit_by_guid(player_mgr, guid)
 
             if not flight_master:
                 return 0
 
             result = ActivateTaxiReplies.ERR_TAXIOK
 
-            if world_session.player_mgr.in_combat:
+            if player_mgr.in_combat:
                 result = ActivateTaxiReplies.ERR_TAXIPLAYERBUSY
-            elif ActivateTaxiHandler._is_taxi_blocked_by_shapeshift(world_session.player_mgr):
+            elif ActivateTaxiHandler._is_blocked_by_shapeshift(player_mgr):
                 result = ActivateTaxiReplies.ERR_TAXIPLAYERSHAPESHIFTED
-            elif world_session.player_mgr.mount_display_id > 0:
+            elif player_mgr.mount_display_id > 0:
                 result = ActivateTaxiReplies.ERR_TAXIPLAYERALREADYMOUNTED
 
             taxi_path = None
@@ -40,15 +36,23 @@ class ActivateTaxiHandler:
                 if not taxi_path:
                     result = ActivateTaxiReplies.ERR_TAXINOSUCHPATH
 
-            if result == ActivateTaxiReplies.ERR_TAXIOK and world_session.player_mgr.coinage < taxi_path.Cost:
+            if result == ActivateTaxiReplies.ERR_TAXIOK and player_mgr.coinage < taxi_path.Cost:
                 result = ActivateTaxiReplies.ERR_TAXINOTENOUGHMONEY
 
             if result == ActivateTaxiReplies.ERR_TAXIOK:
-                world_session.player_mgr.mod_money(-taxi_path.Cost)
-                world_session.player_mgr.taxi_manager.begin_taxi_flight(taxi_path, start_node, dest_node,
-                                                                        flight_master=flight_master)
+                player_mgr.mod_money(-taxi_path.Cost)
+                player_mgr.taxi_manager.begin_taxi_flight(taxi_path, start_node, dest_node, flight_master=flight_master)
 
             data = pack('<I', result)
-            world_session.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_ACTIVATETAXIREPLY, data))
+            player_mgr.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_ACTIVATETAXIREPLY, data))
 
         return 0
+
+    @staticmethod
+    def _is_blocked_by_shapeshift(player_mgr):
+        if not player_mgr.shapeshift_form:
+            return False
+
+        form_entry = DbcDatabaseManager.spell_shapeshift_form_get_by_id(player_mgr.shapeshift_form)
+        # Stances are shapeshifts in dbc but should not block taxi usage.
+        return not (form_entry and (form_entry.Flags & 1))
