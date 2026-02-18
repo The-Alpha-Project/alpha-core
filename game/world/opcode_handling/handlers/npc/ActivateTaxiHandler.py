415722c352b48de966f7965ac05ce5bcf03a4ca7
devw4r <108442943+devw4r@users.noreply.github.com>
2026-02-17 22:41:54 -0600

    Update ActivateTaxiHandler.py

--- game/world/opcode_handling/handlers/npc/ActivateTaxiHandler.py
+++ game/world/opcode_handling/handlers/npc/ActivateTaxiHandler.py
@@ -2,7 +2,7 @@ from struct import unpack, pack
 from database.dbc.DbcDatabaseManager import DbcDatabaseManager
 from game.world.opcode_handling.HandlerValidator import HandlerValidator
 from network.packet.PacketWriter import PacketWriter
-from utils.constants.MiscCodes import ActivateTaxiReplies
+from utils.constants.MiscCodes import ActivateTaxiReplies, NpcFlags
 from utils.constants.OpCodes import OpCode
 
 
@@ -18,24 +18,45 @@ class ActivateTaxiHandler:
             guid, start_node, dest_node = unpack('<Q2I', reader.data[:16])
             flight_master = player_mgr.get_map().get_surrounding_unit_by_guid(player_mgr, guid)
 
-            if not flight_master:
-                return 0
-
             result = ActivateTaxiReplies.ERR_TAXIOK
 
-            if player_mgr.in_combat:
+            # "You are too far away from the taxi stand!"
+            if not flight_master:
+                result = ActivateTaxiReplies.ERR_TAXITOOFARAWAY
+            # "There is no taxi vendor nearby!"
+            elif (flight_master.get_npc_flags() & NpcFlags.NPC_FLAG_FLIGHTMASTER) == 0:
+                result = ActivateTaxiReplies.ERR_TAXINOVENDORNEARBY
+            # "You are too far away from the taxi stand!"
+            elif not flight_master.is_within_interactable_distance(player_mgr):
+                result = ActivateTaxiReplies.ERR_TAXITOOFARAWAY
+            # "You are busy and can't use the taxi service now."
+            elif player_mgr.in_combat:
                 result = ActivateTaxiReplies.ERR_TAXIPLAYERBUSY
+            # "You are in shapeshift form."
             elif ActivateTaxiHandler._is_blocked_by_shapeshift(player_mgr):
                 result = ActivateTaxiReplies.ERR_TAXIPLAYERSHAPESHIFTED
+            # "You are already mounted! Dismount first."
             elif player_mgr.mount_display_id > 0:
                 result = ActivateTaxiReplies.ERR_TAXIPLAYERALREADYMOUNTED
+            # "You are moving."
+            elif player_mgr.is_moving():
+                result = ActivateTaxiReplies.ERR_TAXIPLAYERMOVING
+            # "You are already there!"
+            elif start_node == dest_node:
+                result = ActivateTaxiReplies.ERR_TAXISAMENODE
+            # "You haven't reached that taxi node on foot yet!"
+            elif not ActivateTaxiHandler._is_known_taxi_node(player_mgr, start_node) \
+                    or not ActivateTaxiHandler._is_known_taxi_node(player_mgr, dest_node):
+                result = ActivateTaxiReplies.ERR_TAXINOTVISITED
 
             taxi_path = None
             if result == ActivateTaxiReplies.ERR_TAXIOK:
                 taxi_path = DbcDatabaseManager.taxi_path_get(start_node, dest_node)
+                # "There is no direct path to that destination!"
                 if not taxi_path:
                     result = ActivateTaxiReplies.ERR_TAXINOSUCHPATH
 
+            # "You don't have enough money!"
             if result == ActivateTaxiReplies.ERR_TAXIOK and player_mgr.coinage < taxi_path.Cost:
                 result = ActivateTaxiReplies.ERR_TAXINOTENOUGHMONEY
 
@@ -47,6 +68,13 @@ class ActivateTaxiHandler:
             player_mgr.enqueue_packet(PacketWriter.get_packet(OpCode.SMSG_ACTIVATETAXIREPLY, data))
 
         return 0
+
+    @staticmethod
+    def _is_known_taxi_node(player_mgr, taxi_node):
+        known_nodes_count = len(player_mgr.taxi_manager.available_taxi_nodes)
+        if taxi_node <= 0 or taxi_node > known_nodes_count:
+            return False
+        return player_mgr.taxi_manager.has_node(taxi_node)
 
     @staticmethod
     def _is_blocked_by_shapeshift(player_mgr):
